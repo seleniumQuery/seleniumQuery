3c6249b4936594b37769805dc862ee24b0d298ae
acdcjunior <acdcjunior@gmail.com>
2014-04-05 17:22:28 -0300

    Removed exception added warning for text() on invisible elements

--- src/main/java/org/openqa/selenium/seleniumquery/SeleniumQueryObject.java
+++ src/main/java/org/openqa/selenium/seleniumquery/SeleniumQueryObject.java
@@ -183,6 +183,10 @@ public class SeleniumQueryObject implements Iterable<WebElement> {
 	 * @since 0.2.0
 	 */
 	public String text() {
+		// Warning!
+		// It is impossible to read text from hidden elements in Selenium:
+		// Since a user cannot read text in a hidden element, WebDriver will not allow access to it as well.
+		// More in WebDriver FAQs: https://code.google.com/p/selenium/wiki/FrequentlyAskedQuestions#Q:_Why_is_it_not_possible_to_interact_with_hidden_elements?
 		return TextFunction.text(this.elements);
 	}
 	
