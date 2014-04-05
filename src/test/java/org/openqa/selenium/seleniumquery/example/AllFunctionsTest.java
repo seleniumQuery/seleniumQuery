4a6d66860b70cf50543c43673f96c4f0b1c96ad4
acdcjunior <acdcjunior@gmail.com>
2014-04-05 19:15:03 -0300

    Updated test functions for our lazy-include-everything test class

--- src/test/java/org/openqa/selenium/seleniumquery/example/AllFunctionsTest.java
+++ src/test/java/org/openqa/selenium/seleniumquery/example/AllFunctionsTest.java
@@ -1,11 +1,13 @@
 package org.openqa.selenium.seleniumquery.example;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 import static org.openqa.selenium.seleniumquery.SeleniumQuery.$;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.seleniumquery.TestInfrastructure;
+import org.openqa.selenium.seleniumquery.wait.SeleniumQueryWaitException;
 
 public class AllFunctionsTest {
 
@@ -17,36 +19,40 @@ public class AllFunctionsTest {
 	
 	@Test
 	public void isPresent() {
-		assertEquals("!visibleDiv!",		 $(".visibleDiv").waitUntil().is().present().text()				);
+		assertEquals("!visibleDiv!", $(".visibleDiv").waitUntil().is().present().text());
 	}
-	
+
 	@Test
 	public void isNotPresent() {
-		assertEquals(null,					 $(".nonExistingDiv").waitUntil().is().not().present().text()		);
+		assertNull($(".nonExistingDiv").waitUntil().is().not().present().text());
 	}
-	
+
 	@Test
 	public void isVisible() {
-		assertEquals("!visibleDiv!",		 $(".visibleDiv").waitUntil().is().visible().text()				);
+		assertEquals("!visibleDiv!", $(".visibleDiv").waitUntil().is().visible().text());
 	}
-	
+
 	@Test
 	public void isNotVisible() {
-		$(".invisibleDiv").waitUntil().is().not().visible();
+		assertEquals("", $(".invisibleDiv").waitUntil().is().not().visible().text());
 	}
-	
+
 	@Test
 	public void isVisibleAndEnabled() {
-		assertEquals("!enabledInput!",		 $(".enabledInput").waitUntil().is().visibleAndEnabled().val()	);
+		assertEquals("!enabledInput!", $(".enabledInput").waitUntil().is().visibleAndEnabled().val());
 	}
-	
+
 	@Test
 	public void containsText() {
-		assertEquals("!visibleDiv!",		 $(".visibleDiv").waitUntil().has().textContaining("isibleDi").val()	);
+		assertEquals("!visibleDiv!", $(".visibleDiv").waitUntil().has().textContaining("isibleDix").val());
+	}
+	
+	@Test(expected=SeleniumQueryWaitException.class)
+	public void waitUntil_has_textContaininig__should_throw_an_exception_after_waiting_for_div_without_the_desired_text() {
+		$(".visibleDiv").waitUntil().has().textContaining("CRAZY TEXT THAT IT DOES NOT CONTAIN");
 	}
 	
 	public void queryUntil() {
-		
 		$(".myDivs").queryUntil().atLeastOneElement().is().present();
 		$(".myDivs").queryUntil().atLeastOneElement().is().enabled();
 		$(".myDivs").queryUntil().atLeastOneElement().is().visible();
