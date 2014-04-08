018c58f4bd8e4ba4ac78d55c3553c7e0422da717
acdcjunior <acdcjunior@gmail.com>
2014-04-08 18:01:23 -0300

    Added .get(index) and .get() functions

---
+++ src/main/java/org/openqa/selenium/seleniumquery/functions/GetFunction.java
@@ -0,0 +1,25 @@
+package org.openqa.selenium.seleniumquery.functions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.WebElement;
+
+/**
+ * @author acdcjunior
+ * @since 0.4.0
+ */
+public class GetFunction {
+	
+	public static WebElement get(List<WebElement> elements, int index) {
+		if (elements.size() < index) {
+			return null;
+		}
+		return elements.get(index);
+	}
+
+	public static List<WebElement> get(List<WebElement> elements) {
+		return new ArrayList<WebElement>(elements);
+	}
+
+}
