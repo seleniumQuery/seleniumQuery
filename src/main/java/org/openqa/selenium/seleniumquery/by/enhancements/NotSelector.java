0e34cf8d497d135383c68ffd1b3fe758d3656c8c
acdcjunior <acdcjunior@gmail.com>
2014-04-08 13:38:23 -0300

    :not selector now supports up to 3 levels of () nesting
    Still under a regex-based approach

--- src/main/java/org/openqa/selenium/seleniumquery/by/enhancements/NotSelector.java
+++ src/main/java/org/openqa/selenium/seleniumquery/by/enhancements/NotSelector.java
@@ -4,14 +4,23 @@ import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.seleniumquery.by.SeleniumQueryBy;
 
 public class NotSelector implements SeleniumQueryEnhancement {
 	
-	private static final String NOT_PATTERN = "(.*)"+":not"+"\\("+"([^)]+)"+"\\)";
-
+	/**
+	 * This is a simple, regex-based approach.
+	 * Allows up to three levels of bracket nesting, which feels enough right now.
+	 * 
+	 * Example of three level nesting: div:not(:not(:not(.c3)))
+	 * 
+	 * In the future, the intention is to switch for a, hopefully, simple, lightweight parser approach.
+	 */
+	private static final String NOT_PATTERN = "(.*?)"+":not"+"\\("+"((?:[^()]*|\\((?:[^()]*|\\([^()]*\\))*\\))*)"+"\\)";
+	
 	@Override
 	public boolean isApplicable(String selector) {
 		return selector.matches(NOT_PATTERN);
@@ -28,8 +37,13 @@ public class NotSelector implements SeleniumQueryEnhancement {
 			effectiveSelector = m.group(1);
 			exclusionSelector = m.group(2);
 		}
-
-		List<WebElement> elementsFound = SeleniumQueryBy.byEnhancedSelector(effectiveSelector).findElements(context);
+		
+		List<WebElement> elementsFound = null;
+		if (effectiveSelector.isEmpty()) {
+			elementsFound = new By.ByCssSelector("*").findElements(context);
+		} else {
+			elementsFound = SeleniumQueryBy.byEnhancedSelector(effectiveSelector).findElements(context);
+		}
 
 		List<WebElement> exclusionElements = SeleniumQueryBy.byEnhancedSelector(exclusionSelector).findElements(context);
 		elementsFound.removeAll(exclusionElements);
--- src/test/java/org/openqa/selenium/seleniumquery/by/enhancement/NotSelectorTest.java
+++ src/test/java/org/openqa/selenium/seleniumquery/by/enhancement/NotSelectorTest.java
@@ -4,17 +4,21 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import static org.openqa.selenium.seleniumquery.SeleniumQuery.$;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.seleniumquery.TestInfrastructure;
 
 public class NotSelectorTest {
+	
+	@Before
+	public void setUp() {
+		$.browser.setDefaultDriver(TestInfrastructure.getDriver());
+
+		$.location.href(TestInfrastructure.getHtmlTestFileUrl(getClass()));
+	}
 
     @Test
     public void test() throws Exception {
-        $.browser.setDefaultDriver(TestInfrastructure.getDriver());
-        
-        $.location.href(TestInfrastructure.getHtmlTestFileUrl(getClass()));
-        
         assertThat($("div").size(), is(3));
         assertThat($("div.c1").size(), is(1));
         assertThat($("div.c2").size(), is(1));
@@ -24,6 +28,14 @@ public class NotSelectorTest {
         assertThat($("div:not(.c2)").size(), is(2));
         assertThat($("div:not(.c3)").size(), is(2));
         assertThat($("div:not(.w00t)").size(), is(1));
+    }
+    
+    @Test
+    public void nesting_up_to_two_levels() {
+        assertThat($("div.c3").size(), is(1));
+        assertThat($("div:not(.c3)").size(), is(2));
+        assertThat($("div:not(:not(.c3))").size(), is(1));
+        assertThat($("div:not(:not(:not(.c3)))").size(), is(2));
     }
 
 }
