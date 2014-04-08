d5e7c6fdc62e639cf5d9058a1626e74a04038be6
acdcjunior <acdcjunior@gmail.com>
2014-04-08 13:38:42 -0300

    Changed browser/driver used in the tests

--- src/test/java/org/openqa/selenium/seleniumquery/TestInfrastructure.java
+++ src/test/java/org/openqa/selenium/seleniumquery/TestInfrastructure.java
@@ -1,10 +1,11 @@
 package org.openqa.selenium.seleniumquery;
 
-import static org.openqa.selenium.seleniumquery.SeleniumQuery.$;
-
 import java.io.File;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import com.gargoylesoftware.htmlunit.BrowserVersion;
 
 public class TestInfrastructure {
 	
@@ -15,7 +16,11 @@ public class TestInfrastructure {
 	 * This method exists so we can, from time to time, run all tests in a different browser.
 	 */
 	public static WebDriver getDriver() {
-		return $.browser.getDefaultDriver();
+		@SuppressWarnings("deprecation")
+		HtmlUnitDriver htmlUnitDriver = new HtmlUnitDriver(BrowserVersion.FIREFOX_3_6);
+		
+		htmlUnitDriver.setJavascriptEnabled(true);
+		return htmlUnitDriver;
 	}
 	
 	public static String getHtmlTestFileUrl(Class<?> clazz) {
