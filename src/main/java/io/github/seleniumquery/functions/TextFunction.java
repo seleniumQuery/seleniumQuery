38f5ba17874b433d9062f0de5a6075a7bf9e07cc
ricardo-sc <ricardosc@gmail.com>
2014-05-14 15:02:36 -0300

    .text() function was not concatenating all texts

--- src/main/java/io/github/seleniumquery/functions/TextFunction.java
+++ src/main/java/io/github/seleniumquery/functions/TextFunction.java
@@ -7,14 +7,15 @@ import org.openqa.selenium.WebElement;
 public class TextFunction {
 	
 	public static String text(List<WebElement> elements) {
-		if (elements.isEmpty()) {
-			return null;
+		StringBuilder sb = new StringBuilder();
+		for (WebElement element : elements) {
+			// Warning!
+			// It is impossible to read text from hidden elements in Selenium:
+			// Since a user cannot read text in a hidden element, WebDriver will not allow access to it as well.
+			// More in WebDriver FAQs: https://code.google.com/p/selenium/wiki/FrequentlyAskedQuestions#Q:_Why_is_it_not_possible_to_interact_with_hidden_elements?
+			sb.append(element.getText());
 		}
-		// Warning!
-		// It is impossible to read text from hidden elements in Selenium:
-		// Since a user cannot read text in a hidden element, WebDriver will not allow access to it as well.
-		// More in WebDriver FAQs: https://code.google.com/p/selenium/wiki/FrequentlyAskedQuestions#Q:_Why_is_it_not_possible_to_interact_with_hidden_elements?
-		return elements.get(0).getText();
+		return sb.toString();
 	}
 	
 }
