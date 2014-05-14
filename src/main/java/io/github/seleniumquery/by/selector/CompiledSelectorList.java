dcaf75579852e0d3d27b518ba6baa477d8aa7b7a
acdcjunior <acdcjunior@gmail.com>
2014-05-14 20:21:02 -0300

    Implemented list merging for combination of selectors

--- src/main/java/io/github/seleniumquery/by/selector/CompiledSelectorList.java
+++ src/main/java/io/github/seleniumquery/by/selector/CompiledSelectorList.java
@@ -1,30 +1,28 @@
 package io.github.seleniumquery.by.selector;
 
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.Set;
 
 import org.openqa.selenium.SearchContext;
 import org.openqa.selenium.WebElement;
 
 public class CompiledSelectorList {
 	
-	public List<CompiledSelector> css;
+	List<CompiledSelector> css;
 	
 	CompiledSelectorList(List<CompiledSelector> css) {
 		this.css = css;
 	}
 	
 	public List<WebElement> execute(SearchContext context) {
-		List<List<WebElement>> elements = new ArrayList<List<WebElement>>(css.size());
-		List<WebElement> execute = null;
+		Set<WebElement> elements = new LinkedHashSet<WebElement>();
 		for (CompiledSelector cs : css) {
-			execute = cs.execute(context);
-			elements.add(execute);
+			List<WebElement> execute = cs.execute(context);
+			elements.addAll(execute);
 		}
-		// you have to implement the intersection here!
-		System.err.println("If you used \"selector1,selector2,selector3\" only the result of \"selector1\"" +
-				" is being shown. The rest is yet not implemented!");
-		return execute;
+		return new ArrayList<WebElement>(elements);
 	}
 
 }
