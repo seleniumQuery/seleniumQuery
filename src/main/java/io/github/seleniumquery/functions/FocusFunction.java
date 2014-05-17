534b3bf941aef1001fee66a359e4aa42b8ccd5fd
acdcjunior <acdcjunior@gmail.com>
2014-05-17 16:01:40 -0300

    Add debugging version of .focus()

--- src/main/java/io/github/seleniumquery/functions/FocusFunction.java
+++ src/main/java/io/github/seleniumquery/functions/FocusFunction.java
@@ -19,32 +19,51 @@ public class FocusFunction {
 	}
 
 	private static void focus(WebDriver driver, WebElement elementToBeActive) {
-		System.out.println("@$ focusing "+print(elementToBeActive));
-		System.out.println("@$ current focus is "+printActive(driver));
-		
-		System.out.println("@! Attempting to move");
-		new Actions(driver).moveToElement(elementToBeActive).click().perform();
-		System.out.println("@! after move is "+printActive(driver));
-		
-		elementToBeActive.sendKeys(Keys.TAB);
-		System.out.println("@$ sent tab. current focus is "+printActive(driver));
-
-		WebElement activeElement = driver.switchTo().activeElement();
-		if (activeElement.equals(elementToBeActive)) {
-			System.out.println("@$ wanted is already active. ending");
-			return;
-		}
-		activeElement.sendKeys(Keys.SHIFT, Keys.TAB);
-		System.out.println("@$ sent shift+tab. current focus is "+printActive(driver));
-//		driver.findElement(By.Id(Id of the button)).sendKeys(Keys.Tab) 
-//		return TriggerFunction.trigger(caller, elements, "focus");
-	}
-
-	private static String print(WebElement elementToBeActive) {
-		return elementToBeActive.getAttribute("id") + "(tag: "+elementToBeActive.getTagName()+")";
-	}
-	private static String printActive(WebDriver driver) {
-		return print(driver.switchTo().activeElement());
+//		System.out.println();
+//		System.out.println("@$ Begun attempting to .focus() -> "+print(elementToBeActive));
+//		System.out.println("@$\t Current focus is -> "+printActive(driver));
+//
+//		{	
+//			System.out.println("@$ First attempt, .moveToElement(elementToFocus)!");
+//			new Actions(driver).moveToElement(elementToBeActive).perform();
+//			System.out.println("@$\t After MOVE focus is "+printActive(driver));
+//			if (driver.switchTo().activeElement().equals(elementToBeActive)) {
+//				System.out.println("@$\t After MOVE wanted is already active. Ending: MOVE!");
+//				return;
+//			}
+//		}
+//		
+//		{
+//			System.out.println("@$ Second attempt, .sendKeys(NULL)!");
+			elementToBeActive.sendKeys(Keys.NULL);
+//			System.out.println("@$\t After .sendKeys(NULL) focus is "+printActive(driver));
+//			if (driver.switchTo().activeElement().equals(elementToBeActive)) {
+//				System.out.println("@$\t After .sendKeys(NULL) wanted is already active. Ending: .sendKeys(NULL)!");
+//				return;
+//			}
+//		}
+//		
+//		
+//		System.out.println("@$ Third attempt, .sendKeys(TAB)!");
+//		elementToBeActive.sendKeys(Keys.TAB);
+//		System.out.println("@$ sent tab. current focus is "+printActive(driver));
+//
+//		WebElement activeElement = driver.switchTo().activeElement();
+//		if (activeElement.equals(elementToBeActive)) {
+//			System.out.println("@$ wanted is already active. ending");
+//			return;
+//		}
+//		activeElement.sendKeys(Keys.SHIFT, Keys.TAB);
+//		System.out.println("@$ sent shift+tab. current focus is "+printActive(driver));
+////		driver.findElement(By.Id(Id of the button)).sendKeys(Keys.Tab) 
+////		return TriggerFunction.trigger(caller, elements, "focus");
+//	}
+//
+//	private static String print(WebElement elementToBeActive) {
+//		return elementToBeActive.getAttribute("id") + "(tag: "+elementToBeActive.getTagName()+")";
+//	}
+//	private static String printActive(WebDriver driver) {
+//		return print(driver.switchTo().activeElement());
 	}
 
 }
