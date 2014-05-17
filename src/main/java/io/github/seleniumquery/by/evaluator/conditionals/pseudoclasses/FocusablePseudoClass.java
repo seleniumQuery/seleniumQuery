46513ffbe9a6b4e08290d7e8d7312fe3ed652622
acdcjunior <acdcjunior@gmail.com>
2014-05-17 15:32:53 -0300

    Updated docs for :focusable

--- src/main/java/io/github/seleniumquery/by/evaluator/conditionals/pseudoclasses/FocusablePseudoClass.java
+++ src/main/java/io/github/seleniumquery/by/evaluator/conditionals/pseudoclasses/FocusablePseudoClass.java
@@ -12,23 +12,19 @@ import org.w3c.css.sac.Selector;
 /**
  * @see http://api.jqueryui.com/focusable-selector/
  * 
- * 
  * Some elements are natively focusable, while others require explicitly setting a tab index. In all cases, the element must be visible in order to be focusable.
  * 
- * Elements of the following type are focusable if they are not disabled:
- * - If VISIBLE and NOT DISABLED:
- * 	- input,
- * 	- select,
- * 	- textarea,
- *  - button.
- * 
- * - Anchors are focusable if they have an href or tabindex attribute.
- * 
+ * Elements of the following type are focusable:
+ * - input, if not disabled;
+ * - select, if not disabled;
+ * - textarea, if not disabled;
+ * - button if not disabled;
+ * - anchors, if they have an href or tabindex attribute.
  * - area elements are focusable if they are inside a named map, have an href attribute, and there is a visible image using the map.
- * 
- *  All other elements are focusable based solely on their tabindex attribute and visibility.
+ * - ALL OTHER elements are focusable based solely on their tabindex attribute and visibility.
  * 
  * @author acdcjunior
+ * @since 1.0.0
  */
 public class FocusablePseudoClass implements PseudoClass {
 	
@@ -56,7 +52,7 @@ public class FocusablePseudoClass implements PseudoClass {
 		if (element.getTagName().equals("a") && element.getAttribute("href") != null) {
 			return true;
 		}
-		if (element.getTagName().equals("area") && element.getAttribute("href") != null /* && are inside a named map */ /* && there is a visible image using the map */) {
+		if (element.getTagName().equals("area") && element.getAttribute("href") != null /* && inside a named map */ /* && there is a visible image using the map */) {
 			return true;
 		}
 		return element.getAttribute("tabindex") != null;
