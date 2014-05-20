2aab869720cb6f78b12ce246613fd8c3851e1ecc
acdcjunior <acdcjunior@gmail.com>
2014-05-20 18:12:57 -0300

    Added functions for Firefox, IE and Chrome directly

--- src/main/java/io/github/seleniumquery/globalfunctions/SeleniumQueryDefaultBrowser.java
+++ src/main/java/io/github/seleniumquery/globalfunctions/SeleniumQueryDefaultBrowser.java
@@ -1,17 +1,25 @@
 package io.github.seleniumquery.globalfunctions;
 
+import io.github.seleniumquery.SeleniumQueryConfig;
+
 import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Constructor;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openqa.selenium.WebDriver;
-import io.github.seleniumquery.SeleniumQueryConfig;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 /**
+ * The seleniumQuery browser. Adds several utility functions to the WebDriver class.
+ * 
  * @author acdcjunior
- * @since 0.2.0
+ * @since 1.0.0
  */
 public class SeleniumQueryDefaultBrowser {
 	
@@ -25,6 +33,100 @@ public class SeleniumQueryDefaultBrowser {
 		this.defaultDriver = defaultDriver;
 		this.setDriverTimeout();
 		return this;
+	}
+	
+	/**
+	 * Sets Chrome as the default driver for seleniumQuery.
+	 * <p>
+	 * Note that, as Chrome needs a "server" to bridge selenium to the browser, you have
+	 * to point the path to it. It is a file usually named "chromedriver.exe" and its latest
+	 * version can be downloaded from http://chromedriver.storage.googleapis.com/index.html.
+	 * </p>
+	 * <p>
+	 * For more info, check https://code.google.com/p/selenium/wiki/ChromeDriver
+	 * </p>
+	 * 
+	 * @param pathToChromeDriverExe The full path to the chromedriver.exe file.
+	 */
+	public SeleniumQueryDefaultBrowser setDefaultDriverAsChrome(String pathToChromeDriverExe) {
+		try {
+			File chromeDriverExeFile = new File(pathToChromeDriverExe);
+			String chromeDriverExeFilePath = chromeDriverExeFile.getCanonicalPath();
+			if (!chromeDriverExeFile.exists()) {
+				throw new RuntimeException("No Chrome Driver Server file was found at '"+chromeDriverExeFilePath
+						+"'. Download the latest release at http://chromedriver.storage.googleapis.com/index.html " +
+						"and place it there or specify a different path using " +
+						"$.browser.setDefaultDriverAsChrome(\"other/path/to/chromedriver.exe\").");
+			}
+			System.setProperty("webdriver.chrome.driver", chromeDriverExeFilePath);
+			return setDefaultDriver(new ChromeDriver());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	/**
+	 * This method looks for the chromedriver.exe at the classpath. (Tipically at a resources/ folder of a
+	 * maven project.)
+	 */
+	public SeleniumQueryDefaultBrowser setDefaultDriverAsChrome() {
+		URL chromeDriverResource = getClass().getResource("/chromedriver.exe");
+		String pathToChromeDriver = null;
+		if (chromeDriverResource == null) {
+			pathToChromeDriver = getClass().getResource("/").getPath()+"/chromedriver.exe";
+		} else {
+			pathToChromeDriver = chromeDriverResource.getPath();
+		}
+		return setDefaultDriverAsChrome(pathToChromeDriver);
+	}
+	
+	/**
+	 * This method looks for the IEDriverServer.exe at the classpath. (Tipically at a resources/ folder of a
+	 * maven project.)
+	 */
+	public SeleniumQueryDefaultBrowser setDefaultDriverAsIE() {
+		URL ieDriverServerResource = getClass().getResource("/IEDriverServer.exe");
+		String pathToIeDriverServer = null;
+		if (ieDriverServerResource == null) {
+			pathToIeDriverServer = getClass().getResource("/").getPath()+"/IEDriverServer.exe";
+		} else {
+			pathToIeDriverServer = ieDriverServerResource.getPath();
+		}
+		return setDefaultDriverAsIE(pathToIeDriverServer);
+	}
+	
+	/**
+	 * Sets IE as the default driver for seleniumQuery.
+	 * <p>
+	 * Note that, as IE needs a "server" to bridge selenium to the browser, you have
+	 * to point the path to it. It is a file usually named "IEDriverServer.exe" and its latest
+	 * version can be downloaded from http://selenium-release.storage.googleapis.com/index.html.
+	 * </p>
+	 * <p>
+	 * For more info, check https://code.google.com/p/selenium/wiki/InternetExplorerDriver
+	 * </p>
+	 * 
+	 * @param pathToIEDriverServerExe The full path to the IEDriverServer.exe file.
+	 */
+	public SeleniumQueryDefaultBrowser setDefaultDriverAsIE(String pathToIEDriverServerExe) {
+		try {
+			File ieDriverServerExeFile = new File(pathToIEDriverServerExe);
+			String ieDriverServerExeFilePath = ieDriverServerExeFile.getCanonicalPath();
+			if (!ieDriverServerExeFile.exists()) {
+				throw new RuntimeException("No IE Driver Server file was found at '"+ieDriverServerExeFilePath
+						+"'. Download the latest release at http://selenium-release.storage.googleapis.com/index.html " +
+						"and place it there or specify a different path using " +
+						"$.browser.setDefaultDriverAsIE(\"other/path/to/IEDriverServer.exe\").");
+			}
+			System.setProperty("webdriver.ie.driver", ieDriverServerExeFilePath);
+			return setDefaultDriver(new InternetExplorerDriver());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	
+	public SeleniumQueryDefaultBrowser setDefaultDriverAsFirefox() {
+		return setDefaultDriver(new FirefoxDriver());
 	}
 	
 	private void setDriverTimeout() {
@@ -45,16 +147,20 @@ public class SeleniumQueryDefaultBrowser {
 	private void initializeHtmlUnitDefaultDriver() {
 		try {
 			LOGGER.debug("No default browser was set. Attempting to initialize HtmlUnitDriver.");
-			Class<?> clazz = Class.forName("org.openqa.selenium.htmlunit.HtmlUnitDriver");
-			Constructor<?> constructor = clazz.getConstructor(Boolean.TYPE);
-			WebDriver driver = (WebDriver) constructor.newInstance(true);
-			setDefaultDriver(driver);
+			initializeDriverByClassName("org.openqa.selenium.htmlunit.HtmlUnitDriver");
 			LOGGER.debug("Initialized HtmlUnitDriver as default driver.");
 		} catch (Exception e) {
 			throw new RuntimeException("No HtmlUnitDriver was found on the classpath. Please set " +
 					"the global default driver for seleniumQuery -- the $() functions -- through " +
 					"$.browser.setDefaultDriver(YourDriverClassInstance) before using it.");
 		}
+	}
+
+	private SeleniumQueryDefaultBrowser initializeDriverByClassName(final String className) throws Exception {
+		Class<?> clazz = Class.forName(className);
+		Constructor<?> constructor = clazz.getConstructor(Boolean.TYPE);
+		WebDriver driver = (WebDriver) constructor.newInstance(true);
+		return setDefaultDriver(driver);
 	}
 
 	public void quitDefaultBrowser() {
