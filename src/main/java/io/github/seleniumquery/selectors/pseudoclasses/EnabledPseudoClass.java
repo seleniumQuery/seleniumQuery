aecf7015cdb7477ce369ff140cab742f1cf278cb
acdcjunior <acdcjunior@gmail.com>
2014-05-24 01:09:22 -0300

    Also fixes #3 - this one fixes :enabled

--- src/main/java/io/github/seleniumquery/selectors/pseudoclasses/EnabledPseudoClass.java
+++ src/main/java/io/github/seleniumquery/selectors/pseudoclasses/EnabledPseudoClass.java
@@ -3,6 +3,7 @@ package io.github.seleniumquery.selectors.pseudoclasses;
 import io.github.seleniumquery.selector.CompiledCssSelector;
 import io.github.seleniumquery.selector.CssFilter;
 import io.github.seleniumquery.selector.DriverSupportService;
+import io.github.seleniumquery.selector.SelectorUtils;
 
 import java.util.Arrays;
 import java.util.List;
@@ -21,24 +22,39 @@ public class EnabledPseudoClass implements PseudoClass {
 	private static final String ENABLED_PSEUDO_CLASS_NO_COLON = "enabled";
 	private static final String ENABLED_PSEUDO_CLASS = ":"+ENABLED_PSEUDO_CLASS_NO_COLON;
 	
-	private static final List<String> ALLOWED_TAGS = Arrays.asList("input", "button", "optgroup", "option", "select", "textarea");
+	private static final String OPTGROUP = "optgroup";
+	private static final String OPTION = "option";
+	public static final List<String> ENABLEABLE_TAGS = Arrays.asList("input", "button", OPTGROUP, OPTION, "select", "textarea");
 	
 	@Override
 	public boolean isApplicable(String pseudoClassValue) {
 		return ENABLED_PSEUDO_CLASS_NO_COLON.equals(pseudoClassValue);
 	}
 	
-	
 	@Override
 	public boolean isPseudoClass(WebDriver driver, WebElement element, PseudoClassSelector pseudoClassSelector) {
-		return element.isEnabled() && ALLOWED_TAGS.contains(element.getTagName());
+		// #Cross-Driver
+		// When there is a not disabled <option> under a disabled <optgroup>, HtmlUnitDriver considers
+		// the <option> to be enabled, when it is not
+		if (DriverSupportService.isHtmlUnitDriver(driver) && OPTION.equals(element.getTagName())) {
+			WebElement optionParent = SelectorUtils.parent(element);
+			if (OPTGROUP.equals(optionParent.getTagName()) && !optionParent.isEnabled()) {
+				return false;
+			}
+		}
+		return element.isEnabled() && ENABLEABLE_TAGS.contains(element.getTagName());
 	}
 	
 	private static final CssFilter enabledPseudoClassFilter = new PseudoClassFilter(getInstance());
+	
+	// #Cross-Driver
+	// HtmlUnitDriver has problems with :enabled, so we consider it can never be handler by the browser
+	// by "problems" we mean it is inconsistent, changing depending on what browser it is attempting to emulate
 	@Override
 	public CompiledCssSelector compilePseudoClass(WebDriver driver, PseudoClassSelector pseudoClassSelector) {
 		// https://developer.mozilla.org/en-US/docs/Web/CSS/:enabled
-		if (DriverSupportService.getInstance().supportsNatively(driver, ENABLED_PSEUDO_CLASS)) {
+		if (DriverSupportService.isNotHtmlUnitDriver(driver) &&
+				DriverSupportService.getInstance().supportsNatively(driver, ENABLED_PSEUDO_CLASS)) {
 			return CompiledCssSelector.createNoFilterSelector(ENABLED_PSEUDO_CLASS);
 		}
 		return CompiledCssSelector.createFilterOnlySelector(enabledPseudoClassFilter);
