9524371c10ea51fc66659fb3f4150409634194cf
acdcjunior <acdcjunior@gmail.com>
2014-06-17 19:16:58 -0300

    Added new workaround for HtmlUnit's .innerHTML

--- src/main/java/io/github/seleniumquery/functions/HtmlFunction.java
+++ src/main/java/io/github/seleniumquery/functions/HtmlFunction.java
@@ -3,11 +3,16 @@ package io.github.seleniumquery.functions;
 import io.github.seleniumquery.SeleniumQueryObject;
 import io.github.seleniumquery.selector.DriverSupportService;
 
+import java.lang.reflect.Method;
 import java.util.List;
 
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
+import com.gargoylesoftware.htmlunit.javascript.host.html.HTMLElement;
 
 public class HtmlFunction {
 
@@ -22,23 +27,45 @@ public class HtmlFunction {
 		return html(driver, elements.get(0));
 	}
 
-	// #Cross-Driver
-	// element.getAttribute("innerHTML") does not work for HtmlUnitDriver, so we must
-	// resort to JavaScript when it is being used as driver.
 	public static String html(WebDriver driver, WebElement element) {
 		if (DriverSupportService.isNotHtmlUnitDriver(driver)) {
 			return element.getAttribute("innerHTML");
 		}
-		JavascriptExecutor js = (JavascriptExecutor) driver;
-		String html = js.executeScript("return arguments[0].innerHTML", element).toString();
+		String html = getHtmlUnitInnerHTML(driver, element);
+		
 		// #Cross-Driver
 		// HtmlUnitDriver does not append a "\n" to the HTML of the body tag
 		// as Chrome, Firefox and IE10 seem to.
 		// So we add it!
-		if (DriverSupportService.isHtmlUnitDriver(driver) && "body".equals(element.getTagName())) {
+		if ("body".equals(element.getTagName())) {
 			html = html + "\n";
 		}
 		return html;
+	}
+	
+	// #Cross-Driver
+	// element.getAttribute("innerHTML") does not work for HtmlUnitDriver, so...
+	private static String getHtmlUnitInnerHTML(WebDriver driver, WebElement element) {
+		// we resort to JavaScript when it is being used as driver.
+		if (((HtmlUnitDriver) driver).isJavascriptEnabled()) {
+			JavascriptExecutor js = (JavascriptExecutor) driver;
+			return js.executeScript("return arguments[0].innerHTML", element).toString();
+		}
+		
+		// or use reflection if JS is not enabled
+		// (this method is not preferred as it relies on HtmlUnit's internals, which can change without notice)
+		try {
+			Method method = org.openqa.selenium.htmlunit.HtmlUnitWebElement.class.getDeclaredMethod("getElement");
+			method.setAccessible(true);
+			
+			HtmlElement he = (HtmlElement) method.invoke(element);
+			HTMLElement e = (HTMLElement) he.getScriptObject();
+			
+			return e.getInnerHTML();
+		} catch (Exception e) {
+			System.err.println("Unable to get WebElement's innerHTML: "+element+ "("+e.getMessage()+")");
+			return "";
+		}
 	}
 
 }
