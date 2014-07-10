b2b2efc636f4870f0b89c306491422266333d689
acdcjunior <acdcjunior@gmail.com>
2014-07-10 17:01:15 -0300

    Changed boundary check for .get() function

--- src/main/java/io/github/seleniumquery/functions/GetFunction.java
+++ src/main/java/io/github/seleniumquery/functions/GetFunction.java
@@ -1,18 +1,25 @@
 package io.github.seleniumquery.functions;
 
+import io.github.seleniumquery.globalfunctions.SeleniumQueryDefaultBrowser;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.openqa.selenium.WebElement;
 
 /**
  * @author acdcjunior
- * @since 0.4.0
+ * @since 1.0.0
  */
 public class GetFunction {
 	
+	private static final Log LOGGER = LogFactory.getLog(SeleniumQueryDefaultBrowser.class);
+	
 	public static WebElement get(List<WebElement> elements, int index) {
-		if (elements.size() < index) {
+		if (elements.size() <= index) {
+			LOGGER.warn(".get() called on index ("+index+") larger than current .size() ("+elements.size()+").");
 			return null;
 		}
 		return elements.get(index);
