43e06be01ab015be24cc31d86fc5fb186bf078e5
acdcjunior <acdcjunior@gmail.com>
2014-07-28 15:23:32 -0300

    Updaded logged message for get()

--- src/main/java/io/github/seleniumquery/functions/GetFunction.java
+++ src/main/java/io/github/seleniumquery/functions/GetFunction.java
@@ -19,7 +19,7 @@ public class GetFunction {
 	
 	public static WebElement get(List<WebElement> elements, int index) {
 		if (elements.size() <= index) {
-			LOGGER.warn(".get() called on index larger than or equal to current .size(). Index="+index+"; size="+elements.size()+".");
+			LOGGER.warn(".get() called on index larger than or equal to current .size(). null was returned. Index: "+index+", Size: "+elements.size());
 			return null;
 		}
 		return elements.get(index);
