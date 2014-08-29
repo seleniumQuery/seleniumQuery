b213dd131b9976a7e201a63b32607ef47a0ee38d
acdcjunior <acdcjunior@gmail.com>
2014-08-29 17:49:25 -0300

    Expression merging logic moved to SqSelectorKind

--- src/main/java/io/github/seleniumquery/selectorxpath/SqSelectorKind.java
+++ src/main/java/io/github/seleniumquery/selectorxpath/SqSelectorKind.java
@@ -1,5 +1,6 @@
 package io.github.seleniumquery.selectorxpath;
 
+
 public enum SqSelectorKind {
 	
 	/*
@@ -9,12 +10,59 @@ public enum SqSelectorKind {
 	 * if CONDITIONAL_TO_ALL, then this must be applied to the whole result of the other, such as:
 	 * (//*[@other])[@thisSelector]
 	 */
-	CONDITIONAL_SIMPLE,
-	CONDITIONAL_TO_ALL,
+	CONDITIONAL_SIMPLE {
+		@Override
+		public String merge(String sourceXPathExpression, String otherXPathExpression) {
+			if (sourceXPathExpression.endsWith("]")) {
+				return sourceXPathExpression.substring(0, sourceXPathExpression.length()-1) + " and " + otherXPathExpression.substring(1);
+			}
+			return sourceXPathExpression + otherXPathExpression;
+		}
+	},
+	CONDITIONAL_TO_ALL {
+		@Override
+		public String merge(String sourceXPathExpression, String otherXPathExpression) {
+			return "(" + sourceXPathExpression + ")" + otherXPathExpression;
+		}
+	},
 	
-	DESCENDANT_GENERAL, // "//"
-	DESCENDANT_DIRECT, // "/"
-	ADJACENT, // "/following-sibling::"  (the direct will add a position()=1 by itself)
-	TAG
+	// "//"
+	DESCENDANT_GENERAL {
+		@Override
+		public String merge(String sourceXPathExpression, String otherXPathExpression) {
+			return sourceXPathExpression + "//" + otherXPathExpression;
+		}
+	},
 	
+	// "/"
+	DESCENDANT_DIRECT {
+		@Override
+		public String merge(String sourceXPathExpression, String otherXPathExpression) {
+			return sourceXPathExpression + "/" + otherXPathExpression;
+		}
+	},
+	
+	// "/following-sibling::"  (the direct will add a position()=1 by itself)
+	ADJACENT {
+		@Override
+		public String merge(String sourceXPathExpression, String otherXPathExpression) {
+			return sourceXPathExpression + "/following-sibling::" + otherXPathExpression;
+		}
+	},
+	
+	TAG {
+		@Override
+		public String merge(String sourceXPathExpression, String otherXPathExpression) {
+			if ("*".equals(otherXPathExpression)) {
+				return CONDITIONAL_SIMPLE.merge(sourceXPathExpression, "[name()]");
+			}
+			return CONDITIONAL_SIMPLE.merge(sourceXPathExpression, "[name() = '"+otherXPathExpression+"']");
+		}
+	};
+	
+	/**
+	 * @return The merged expression.
+	 */
+	public abstract String merge(String sourceXPathExpression, String otherXPathExpression);
+
 }
