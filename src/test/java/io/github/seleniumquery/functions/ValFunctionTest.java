061cf5b0e99eb7e74be1b6b29965bacece89b584
acdcjunior <acdcjunior@gmail.com>
2014-09-03 16:54:49 -0300

    fixes #17 - val() also clears textareas now

--- src/test/java/io/github/seleniumquery/functions/ValFunctionTest.java
+++ src/test/java/io/github/seleniumquery/functions/ValFunctionTest.java
@@ -13,7 +13,7 @@ public class ValFunctionTest {
 	@Rule
 	public SetUpAndTearDownDriver setUpAndTearDownDriverRule = new SetUpAndTearDownDriver();
 
-	// http://jsbin.com/qoqoqugu/1/edit
+	// http://jsbin.com/mihovu/1/edit
     @Test
     public void val_function__getting() throws Exception {
     	assertThat($("div").val(), is(""));
@@ -24,6 +24,34 @@ public class ValFunctionTest {
     	assertThat($("#ta").val().trim(), is("bozo"));
     	assertThat($("#i1").val(), is("ii!"));
     	assertThat($("#r1").val(), is("ra!"));
+    }
+    
+    @Test
+    public void val_function__setting() throws Exception {
+    	// when
+    	assertThat($("div").val(), is(""));
+    	assertThat($("#opt").val(), is("a1"));
+    	assertThat($("#s1").val(), is("a1"));
+    	// Chrome/PhantomJS/IE10: "    bozo\n  "
+    	// HtmlUnit/Firefox: "bozo"
+    	assertThat($("#ta").val().trim(), is("bozo"));
+    	assertThat($("#i1").val(), is("ii!"));
+    	assertThat($("#r1").val(), is("ra!"));
+
+    	$("div").val("SHOULD HAVE NO EFFECT");
+//    	$("#opt").val("NEW-OPTION-VALUE");
+//    	$("#s1").val("NEW-SELECT-VALUE"); // throws exception as this element does not exist
+    	$("#ta").val("NEW-TEXTAREA-VALUE");
+    	$("#i1").val("NEW-INPUT-TEXT-VALUE");
+//    	$("#r1").val("NEW-RADIO-VALUE");
+
+    	// then
+    	assertThat($("div").val(), is("")); // jquery returns the set
+//    	assertThat($("#opt").val(), is("NEW-OPTION-VALUE"));
+//    	assertThat($("#s1").val(), is(nullValue())); // the value is not set, so...
+    	assertThat($("#ta").val(), is("NEW-TEXTAREA-VALUE"));
+    	assertThat($("#i1").val(), is("NEW-INPUT-TEXT-VALUE"));
+//    	assertThat($("#r1").val(), is("NEW-RADIO-VALUE"));
     }
 
 }
