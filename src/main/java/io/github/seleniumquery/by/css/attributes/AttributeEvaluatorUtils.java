646bb287ab42a3603aa1ea3c1f20196b5f98b3a7
acdcjunior <acdcjunior@gmail.com>
2014-11-21 14:25:50 -0200

    Attribute selectors are now case-insensitive - closes #64

--- src/main/java/io/github/seleniumquery/by/css/attributes/AttributeEvaluatorUtils.java
+++ src/main/java/io/github/seleniumquery/by/css/attributes/AttributeEvaluatorUtils.java
@@ -8,6 +8,7 @@ import org.w3c.css.sac.AttributeCondition;
  * Utility methods for XPath attributes.
  * 
  * @author acdcjunior
+ *
  * @since 0.9.0
  */
 public class AttributeEvaluatorUtils {
@@ -15,9 +16,9 @@ public class AttributeEvaluatorUtils {
 	public static String getXPathAttribute(AttributeCondition attributeCondition) {
 		String attributeName = attributeCondition.getLocalName();
 		if (!Character.isLetter(attributeName.charAt(0))) {
-			return "@*[local-name() = "+ SelectorUtils.intoEscapedXPathString(attributeName) +"]";
+			attributeName = SelectorUtils.intoEscapedXPathString(attributeName);
 		}
-		return "@"+ attributeName;
+		return "@*[translate(local-name(), 'ABCDEFGHIJKLMNOPQRSTUVWXYZ', 'abcdefghijklmnopqrstuvwxyz') = '"+ attributeName.toLowerCase() +"']";
 	}
 
 }
