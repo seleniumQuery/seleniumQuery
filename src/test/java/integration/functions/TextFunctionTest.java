ee96b83d512dac6f16514a375ec18a1137d2e440
acdcjunior <acdcjunior@gmail.com>
2014-11-22 00:07:30 -0200

    nothing to change. <textarea>.text() returns original value for jQuery as well, so we're OK! - closes #59

--- src/test/java/integration/functions/TextFunctionTest.java
+++ src/test/java/integration/functions/TextFunctionTest.java
@@ -4,8 +4,11 @@ import infrastructure.junitrule.SetUpAndTearDownDriver;
 import io.github.seleniumquery.by.DriverVersionUtils;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
 
 import static io.github.seleniumquery.SeleniumQuery.$;
+import static io.github.seleniumquery.by.DriverVersionUtils.isHtmlUnitDriver;
 import static io.github.seleniumquery.by.DriverVersionUtils.isHtmlUnitDriverEmulatingIEBelow11;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
@@ -17,18 +20,34 @@ public class TextFunctionTest {
 
     @Test
     public void text_function() {
-    	if (isHtmlUnitDriverEmulatingIEBelow11($.browser.getDefaultDriver())) {
+		WebDriver driver = $.browser.getDefaultDriver();
+
+		if (isHtmlUnitDriverEmulatingIEBelow11(driver)) {
     		assertThat($("div.demo-container").text().replaceAll("\\s+", " "), is("Demonstration Box list item 1list item 2"));
-    	} else if (DriverVersionUtils.isHtmlUnitDriver($.browser.getDefaultDriver())) {
+    	} else if (isHtmlUnitDriver(driver)) {
     			assertThat($("div.demo-container").text(), is("Demonstration Box\nlist item 1 list item 2"));
     	} else {
     		assertThat($("div.demo-container").text(), is("Demonstration Box\nlist item 1\nlist item 2"));
     	}
-    }
-    
-    @Test
-    public void text_function__another_div() {
+
     	assertThat($("div.d").text(), is("Batman Spider Man yo Hulk"));
-    }
+
+    	assertThat($("#myTextArea").text().trim(), is("Initial value for textarea"));
+    	assertThat($("#myTextArea").val().trim(), is("Initial value for textarea"));
+		$("#myTextArea").val("Typed stuff in textarea");
+
+		// #Cross-Driver
+		// this is no fix, just documenting the difference
+		// IE (tested IE11) will make .text() follow the typed text. Other browsers will keep the original one.
+		// This is OK, as it is the SAME behavior presented by jQuery!
+		//
+		// Well, HtmlUnit follows IE...
+		if (driver instanceof InternetExplorerDriver || isHtmlUnitDriver(driver)) {
+    		assertThat($("#myTextArea").text().trim(), is("Typed stuff in textarea"));
+		} else {
+			assertThat($("#myTextArea").text().trim(), is("Initial value for textarea"));
+		}
+    	assertThat($("#myTextArea").val().trim(), is("Typed stuff in textarea"));
+	}
 
 }
