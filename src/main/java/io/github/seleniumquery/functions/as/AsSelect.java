474e7d31ba66151ac263a5abd8dd28a065745bb0
acdcjunior <acdcjunior@gmail.com>
2014-11-23 17:00:10 -0200

    SelectFunction selects only on <select> elements - closes #15

--- src/main/java/io/github/seleniumquery/functions/as/AsSelect.java
+++ src/main/java/io/github/seleniumquery/functions/as/AsSelect.java
@@ -1,26 +1,29 @@
 package io.github.seleniumquery.functions.as;
 
 import io.github.seleniumquery.SeleniumQueryObject;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 
 import java.util.List;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.Select;
+import static io.github.seleniumquery.by.WebElementUtils.isSelectTag;
 
 public class AsSelect {
 	
 	public static SeleniumQueryObject selectOptionByVisibleText(SeleniumQueryObject caller, List<WebElement> elements, String text) {
-		// TODO filter only select items, so no exceptions are thrown below
 		for (WebElement element : elements) {
-			new Select(element).selectByVisibleText(text);
+			if (isSelectTag(element)) {
+				new Select(element).selectByVisibleText(text);
+			}
 		}
 		return caller;
 	}
 	
 	public static SeleniumQueryObject selectOptionByValue(SeleniumQueryObject caller, List<WebElement> elements, String value) {
-		// TODO filter only select items, so no exceptions are thrown below
 		for (WebElement element : elements) {
-			new Select(element).selectByValue(value);
+			if (isSelectTag(element)) {
+				new Select(element).selectByValue(value);
+			}
 		}
 		return caller;
 	}
