056ee7aaeff2db8db24b12f37f15de8896249e36
acdcjunior <acdcjunior@gmail.com>
2014-11-23 14:04:28 -0200

    Removed/moved $.browser.sleep() functions into $.pause() - Also using selenium's PauseAction now.

--- src/main/java/io/github/seleniumquery/SeleniumQueryBrowserFunctions.java
+++ src/main/java/io/github/seleniumquery/SeleniumQueryBrowserFunctions.java
@@ -3,6 +3,7 @@ package io.github.seleniumquery;
 import io.github.seleniumquery.globalfunctions.driver.SeleniumQueryDriver;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.openqa.selenium.interactions.PauseAction;
 
 import java.io.File;
 
@@ -41,7 +42,9 @@ public class SeleniumQueryBrowserFunctions {
 
     /**
      * Opens the given URL in the default browser.
+     *
      * @param urlToOpen The URL to be opened. Example: "http://seleniumquery.github.io"
+     * @return A self reference.
      *
      * @since 0.9.0
      */
@@ -53,12 +56,38 @@ public class SeleniumQueryBrowserFunctions {
 
     /**
      * Opens the given file as a URL in the browser.
+     *
      * @param fileToOpenAsURL the file to be opened.
+     * @return A self reference.
      *
      * @since 0.9.0
      */
     public SeleniumQueryBrowserFunctions url(File fileToOpenAsURL) {
         return url(fileToOpenAsURL.toURI().toString());
+    }
+
+    /**
+     * <p>Performs a pause, instructing the the browser (thread) to wait (sleep) for the time
+     * <b>in milliseconds</b> given.</p>
+     * <pre>
+     * $.pause(200); // pauses for 200 milliseconds
+     * $.pause(10 * 1000); // pauses for 10 seconds
+     * </pre>
+     * @param timeToPauseInMillis pause duration, in milliseconds.
+     * @return A self reference.
+     *
+     * @since 0.9.0
+     *
+     * @deprecated 'Pause' is considered to be a bad design practice. It is better to write code based on what the user
+     *      will expect, for that consider using {@code .waitUntil()} functions, such as
+     *      <code>$("#someDivThatShouldComeOut").waitUntil().is(":visible");</code>.
+     */
+    @Deprecated
+    @SuppressWarnings("deprecation")
+    public SeleniumQueryBrowserFunctions pause(long timeToPauseInMillis) {
+        LOGGER.debug(String.format("Pausing for %d milliseconds.", timeToPauseInMillis));
+        new PauseAction(timeToPauseInMillis).perform();
+        return this;
     }
 
 }
--- src/main/java/io/github/seleniumquery/SeleniumQueryDeprecatedBrowser.java
+++ src/main/java/io/github/seleniumquery/SeleniumQueryDeprecatedBrowser.java
@@ -6,84 +6,95 @@ import org.openqa.selenium.WebDriver;
 import java.io.File;
 import java.util.concurrent.TimeUnit;
 
+import static java.util.concurrent.TimeUnit.MILLISECONDS;
+
 /**
  * The seleniumQuery browser. Adds several utility functions to the WebDriver class.
  * 
  * @author acdcjunior
  *
  * @since 0.9.0
+ *
+ * @deprecated This class refers to a deprecated way of accessing some functions and will be removed by the next release.
+ * You'll find the functions for this object in <b>{@code $.function();}</b> or <b>{@code $.driver().function();}</b>.
  */
-@Deprecated
 public class SeleniumQueryDeprecatedBrowser {
 	
 	private SeleniumQueryBrowserFunctions browser;
 	public SeleniumQueryDeprecatedBrowser(SeleniumQueryBrowserFunctions browser) { this.browser = browser; }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.driver();</code> */
-	@Deprecated public SeleniumQueryDriver globalDriver() { return browser.driver(); }
+	/** @deprecated Use: <b><code>$.driver();</code> */
+	public SeleniumQueryDriver globalDriver() { return browser.driver(); }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.driver().use(webDriverInstance);</code> */
-	@Deprecated public SeleniumQueryDeprecatedBrowser setDefaultDriver(WebDriver defaultDriver) { browser.driver().use(defaultDriver); return this; }
+	/** @deprecated Use: <b><code>$.driver().use(webDriverInstance);</code> */
+	public SeleniumQueryDeprecatedBrowser setDefaultDriver(WebDriver defaultDriver) { browser.driver().use(defaultDriver); return this; }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.driver().useHtmlUnit();</code> */
-	@Deprecated public SeleniumQueryDeprecatedBrowser setDefaultDriverAsHtmlUnit() { browser.driver().useHtmlUnit(); return this; }
+	/** @deprecated Use: <b><code>$.driver().useHtmlUnit();</code> */
+	public SeleniumQueryDeprecatedBrowser setDefaultDriverAsHtmlUnit() { browser.driver().useHtmlUnit(); return this; }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.driver().useFirefox();</code> */
-	@Deprecated public SeleniumQueryDeprecatedBrowser setDefaultDriverAsFirefox() { browser.driver().useFirefox(); return this; }
+	/** @deprecated Use: <b><code>$.driver().useFirefox();</code> */
+	public SeleniumQueryDeprecatedBrowser setDefaultDriverAsFirefox() { browser.driver().useFirefox(); return this; }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.driver().useChrome();</code> */
-	@Deprecated public SeleniumQueryDeprecatedBrowser setDefaultDriverAsChrome() { browser.driver().useChrome(); return this; }
+	/** @deprecated Use: <b><code>$.driver().useChrome();</code> */
+	public SeleniumQueryDeprecatedBrowser setDefaultDriverAsChrome() { browser.driver().useChrome(); return this; }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.driver().useChrome().withPathToChromeDriverExe(path);</code> */
-	@Deprecated public SeleniumQueryDeprecatedBrowser setDefaultDriverAsChrome(String path) { browser.driver().useChrome().withPathToChromeDriverExe(path); return this; }
+	/** @deprecated Use: <b><code>$.driver().useChrome().withPathToChromeDriverExe(path);</code> */
+	public SeleniumQueryDeprecatedBrowser setDefaultDriverAsChrome(String path) { browser.driver().useChrome().withPathToChromeDriverExe(path); return this; }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.driver().useInternetExplorer();</code> */
-	@Deprecated public SeleniumQueryDeprecatedBrowser setDefaultDriverAsIE() { browser.driver().useInternetExplorer(); return this; }
+	/** @deprecated Use: <b><code>$.driver().useInternetExplorer();</code> */
+	public SeleniumQueryDeprecatedBrowser setDefaultDriverAsIE() { browser.driver().useInternetExplorer(); return this; }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.driver().useInternetExplorer().withPathToIEDriverServerExe(path);</code> */
-	@Deprecated public SeleniumQueryDeprecatedBrowser setDefaultDriverAsIE(String path) { browser.driver().useInternetExplorer().withPathToIEDriverServerExe(path); return this; }
+	/** @deprecated Use: <b><code>$.driver().useInternetExplorer().withPathToIEDriverServerExe(path);</code> */
+	public SeleniumQueryDeprecatedBrowser setDefaultDriverAsIE(String path) { browser.driver().useInternetExplorer().withPathToIEDriverServerExe(path); return this; }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.driver().usePhantomJS();</code> */
-	@Deprecated public SeleniumQueryDeprecatedBrowser setDefaultDriverAsPhantomJS() { browser.driver().usePhantomJS(); return this; }
+	/** @deprecated Use: <b><code>$.driver().usePhantomJS();</code> */
+	public SeleniumQueryDeprecatedBrowser setDefaultDriverAsPhantomJS() { browser.driver().usePhantomJS(); return this; }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.driver().usePhantomJS().withPathToPhantomJsExe(path);</code> */
-	@Deprecated public SeleniumQueryDeprecatedBrowser setDefaultDriverAsPhantomJS(String path) { browser.driver().usePhantomJS().withPathToPhantomJsExe(path); return this; }
+	/** @deprecated Use: <b><code>$.driver().usePhantomJS().withPathToPhantomJsExe(path);</code> */
+	public SeleniumQueryDeprecatedBrowser setDefaultDriverAsPhantomJS(String path) { browser.driver().usePhantomJS().withPathToPhantomJsExe(path); return this; }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.driver().get();</code> */
-	@Deprecated public WebDriver getDefaultDriver() { return browser.driver().get(); }
+	/** @deprecated Use: <b><code>$.driver().get();</code> */
+	public WebDriver getDefaultDriver() { return browser.driver().get(); }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.driver().quit();</code> */
-	@Deprecated public void quitDefaultDriver() { browser.driver().quit(); }
+	/** @deprecated Use: <b><code>$.driver().quit();</code> */
+	public void quitDefaultDriver() { browser.driver().quit(); }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.driver().quit();</code> */
-	@Deprecated public void quitDefaultBrowser() { browser.driver().quit(); }
+	/** @deprecated Use: <b><code>$.driver().quit();</code> */
+	public void quitDefaultBrowser() { browser.driver().quit(); }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.driver().quit();</code> */
-	@Deprecated public void quit() { browser.driver().quit(); }
+	/** @deprecated Use: <b><code>$.driver().quit();</code> */
+	public void quit() { browser.driver().quit(); }
 	
-	/** This function has been deprecated.<br> Use: <b><code>$.url(url);</code> */
-	@Deprecated public void openUrl(String url) { browser.url(url); }
+	/** @deprecated Use: <b><code>$.url(url);</code> */
+	public void openUrl(String url) { browser.url(url); }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.url(file);</code> */
-	@Deprecated public void openUrl(File file) { browser.url(file); }
+	/** @deprecated Use: <b><code>$.url(file);</code> */
+	public void openUrl(File file) { browser.url(file); }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.url(url);</code> */
-	@Deprecated public void open(String url) { browser.url(url); }
+	/** @deprecated Use: <b><code>$.url(url);</code> */
+	public void open(String url) { browser.url(url); }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.url(file);</code> */
-	@Deprecated public void open(File file) { browser.url(file); }
+	/** @deprecated Use: <b><code>$.url(file);</code> */
+	public void open(File file) { browser.url(file); }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.url(url);</code> */
-	@Deprecated public void url(String url) { browser.url(url); }
+	/** @deprecated Use: <b><code>$.url(url);</code> */
+	public void url(String url) { browser.url(url); }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.url(file);</code> */
-	@Deprecated public void url(File file) { browser.url(file); }
+	/** @deprecated Use: <b><code>$.url(file);</code> */
+	public void url(File file) { browser.url(file); }
 
-	/** This function has been deprecated.<br> Use: <b><code>$.url();</code> */
-	@Deprecated public String url() { return browser.url(); }
+	/** @deprecated Use: <b><code>$.url();</code> */
+	public String url() { return browser.url(); }
 	
-	/** This function has been deprecated.<br> Use: <b><code>$.url();</code> */
-	@Deprecated public String getCurrentUrl() { return browser.url(); }
+	/** @deprecated Use: <b><code>$.url();</code> */
+	public String getCurrentUrl() { return browser.url(); }
+
+	/** @deprecated Use: <b><code>$.pause(timeToPauseInMillis);</code> */
+	public void sleep(int timeToWait, TimeUnit timeUnit) { browser.pause(MILLISECONDS.convert(timeToWait, timeUnit));
+	}
+
+	/** @deprecated Use: <b><code>$.pause(timeToPauseInMillis);</code> */
+	public void sleep(int millis) { browser.pause(millis); }
 
 	/**
 	 * Attempts to maximize the window of the default driver.
@@ -92,35 +103,6 @@ public class SeleniumQueryDeprecatedBrowser {
 	 */
 	public void maximizeWindow() {
 		getDefaultDriver().manage().window().maximize();
-	}
-
-	/**
-	 * Instructs the browser (thread) to wait (sleep) for the time passed as argument.<br>
-	 * <br>
-	 * Example: <code>$.browser.sleep(10, TimeUnit.SECONDS); // sleeps for 10 seconds</code>
-	 *
-	 * @since 0.9.0
-	 */
-	public void sleep(int timeToWait, TimeUnit timeUnit) {
-		try {
-			long timeInMillis = TimeUnit.MILLISECONDS.convert(timeToWait, timeUnit);
-			int millis = (int) Math.min(timeInMillis, Integer.MAX_VALUE); // using .min() to allow a safe casting
-			System.out.println("Sleeping for " + millis + " milliseconds.");
-			Thread.sleep(millis);
-		} catch (InterruptedException e) {
-			throw new RuntimeException(e);
-		}
-	}
-
-	/**
-	 * Instructs the browser (thread) to wait (sleep) for the time <b>in milliseconds</b> passed as argument.<br>
-	 * <br>
-	 * Example: <code>$.browser.sleep(10 * 1000); // sleeps for 10 seconds</code>
-	 *
-	 * @since 0.9.0
-	 */
-	public void sleep(int millis) {
-		sleep(millis, TimeUnit.MILLISECONDS);
 	}
 
 }
