e2c96f27090f31b67c2b27f26233f9e55c7de6a5
acdcjunior <acdcjunior@gmail.com>
2014-11-29 22:44:19 -0200

    .get(idx) wont use SeleniumQueryObject#elements anymore - see #76

--- src/main/java/io/github/seleniumquery/functions/jquery/miscellaneous/GetFunction.java
+++ src/main/java/io/github/seleniumquery/functions/jquery/miscellaneous/GetFunction.java
@@ -1,15 +1,14 @@
 package io.github.seleniumquery.functions.jquery.miscellaneous;
 
+import io.github.seleniumquery.SeleniumQueryObject;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.openqa.selenium.WebElement;
 
-import java.util.ArrayList;
 import java.util.List;
 
 /**
  * <pre>
- * $("selector").get();
  * $("selector").get(index);
  * </pre>
  *
@@ -21,7 +20,8 @@ public class GetFunction {
 	
 	private static final Log LOGGER = LogFactory.getLog(GetFunction.class);
 	
-	public static WebElement get(List<WebElement> elements, int index) {
+	public static WebElement get(SeleniumQueryObject caller, int index) {
+		List<WebElement> elements = caller.get();
 		if (elements.size() <= index) {
 			LOGGER.warn(".get() called on index larger than or equal to current .size(). null was returned. Index: "+index+", Size: "+elements.size());
 			return null;
