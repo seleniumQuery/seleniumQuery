9f0d454e5a3df017fc762e96576dca1711472b8d
acdcjunior <acdcjunior@gmail.com>
2014-11-30 21:55:49 -0200

    $("").submit() function added - closes #78 (see also previous the 3 commits, as the changes to this functions were incorrectly appended to those and I don't have the patience to fix that right now)

--- src/test/java/integration/functions/jquery/forms/SubmitFunctionWithoutJSTest.java
+++ src/test/java/integration/functions/jquery/forms/SubmitFunctionWithoutJSTest.java
@@ -6,7 +6,6 @@ import org.junit.Test;
 
 import static io.github.seleniumquery.SeleniumQuery.$;
 import static org.hamcrest.CoreMatchers.endsWith;
-import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertThat;
 
 public class SubmitFunctionWithoutJSTest {
