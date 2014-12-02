1c928eb7af1505ced988d4142647efdc7fd71d6b
acdcjunior <acdcjunior@gmail.com>
2014-12-02 03:23:23 -0200

    specified conditions where a selector will be understood as an XPath expression

--- src/test/java/io/github/seleniumquery/by/SeleniumQueryByTest.java
+++ src/test/java/io/github/seleniumquery/by/SeleniumQueryByTest.java
@@ -1,20 +1,58 @@
 package io.github.seleniumquery.by;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-
 import org.junit.Test;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class SeleniumQueryByTest {
 
 	@Test
-	public void testIsXPathExpression() {
-		// given
-		String selector = "(//table//a)[1]";
-		// when
-		final boolean isXPath = SeleniumQueryBy.isXPathExpression(selector);
-		// then
-		assertThat(isXPath, is(true));
+	public void isXPathExpression__should_guess_XPath_expressions_correctly() {
+		assertIsXPathExpression("(//table//a)[1]");
+		assertIsXPathExpression("   ((   (   (   (			(			(/html");
+		assertIsXPathExpression("ancestor::*");
+		assertIsXPathExpression("   ((  (ancestor::*");
+		assertIsXPathExpression("ancestor-or-self::*");
+		assertIsXPathExpression("   ((  (ancestor-or-self::*");
+		assertIsXPathExpression("attribute::*");
+		assertIsXPathExpression("   ((  (attribute::*");
+		assertIsXPathExpression("child::*");
+		assertIsXPathExpression("   ((  (child::*");
+		assertIsXPathExpression("descendant::*");
+		assertIsXPathExpression("   ((  (descendant::*");
+		assertIsXPathExpression("descendant-or-self::*");
+		assertIsXPathExpression("   ((  (descendant-or-self::*");
+		assertIsXPathExpression("following::*");
+		assertIsXPathExpression("   ((  (following::*");
+		assertIsXPathExpression("following-sibling::*");
+		assertIsXPathExpression("   ((  (following-sibling::*");
+		assertIsXPathExpression("parent::*");
+		assertIsXPathExpression("   ((  (parent::*");
+		assertIsXPathExpression("preceding::*");
+		assertIsXPathExpression("   ((  (preceding::*");
+		assertIsXPathExpression("preceding-sibling::*");
+		assertIsXPathExpression("   ((  (preceding-sibling::*");
+		assertIsXPathExpression("self::*");
+		assertIsXPathExpression("   ((  (self::*");
+	}
+
+	private void assertIsXPathExpression(String selector) {
+		assertTrue("Selector \""+selector+"\" should be considered an XPathExpression.", SeleniumQueryBy.isXPathExpression(selector));
+	}
+
+	@Test
+	public void isXPathExpression__should_not_confuse_CSS_selectors_with_XPath_expressions() {
+		assertIsNotXPathExpression("   #id");
+		assertIsNotXPathExpression("   .class");
+		assertIsNotXPathExpression("*");
+		assertIsNotXPathExpression("tag");
+		assertIsNotXPathExpression("ancestor"); // XPath axis without :: wont be a XPath axis
+		assertIsNotXPathExpression("tag::xyz");
+	}
+
+	private void assertIsNotXPathExpression(String selector) {
+		assertFalse("Selector \"" + selector + "\" should NOT be considered an XPathExpression.", SeleniumQueryBy.isXPathExpression(selector));
 	}
 
 }
