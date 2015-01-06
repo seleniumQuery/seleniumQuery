58606f41c43c020e9a17f72c69fa6b2d743cbb0b
ricardo-sc <ricardosc@gmail.com>
2015-01-06 19:54:14 -0200

    Added more integration tests for combination selectors

---
+++ src/test/java/io/github/seleniumquery/by/css/combinators/DescendantCssSelectorTest.java
@@ -0,0 +1,29 @@
+package io.github.seleniumquery.by.css.combinators;
+
+import io.github.seleniumquery.by.preparser.ParsedSelector;
+import io.github.seleniumquery.by.preparser.SelectorParser;
+import io.github.seleniumquery.by.xpath.XPathComponent;
+import org.junit.Test;
+import org.w3c.css.sac.DescendantSelector;
+import org.w3c.css.sac.SelectorList;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class DescendantCssSelectorTest {
+
+    DescendantCssSelector descendantCssSelector = new DescendantCssSelector();
+
+    @Test
+    public void testToXPath() throws Exception {
+        // given
+        ParsedSelector<SelectorList> parsedSelector = SelectorParser.parseSelector("a b");
+        DescendantSelector descendantSelector = (DescendantSelector) parsedSelector.getSelector().item(0);
+        // when
+        XPathComponent xPathComponent = descendantCssSelector.toXPath(parsedSelector.getStringMap(), descendantSelector);
+        // then
+        String xPath = xPathComponent.toXPath();
+        assertThat(xPath, is("(.//*[self::a]//*[self::b])"));
+    }
+
+}
