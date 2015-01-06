b28d9ec2f4cf830fb79dedc32ca3b12fb84d2fe9
ricardo-sc <ricardosc@gmail.com>
2015-01-06 19:54:14 -0200

    Added more integration tests for combination selectors

---
+++ src/test/java/io/github/seleniumquery/by/css/combinators/DirectAdjacentCssSelectorTest.java
@@ -0,0 +1,29 @@
+package io.github.seleniumquery.by.css.combinators;
+
+import io.github.seleniumquery.by.preparser.ParsedSelector;
+import io.github.seleniumquery.by.preparser.SelectorParser;
+import io.github.seleniumquery.by.xpath.XPathComponent;
+import org.junit.Test;
+import org.w3c.css.sac.SelectorList;
+import org.w3c.css.sac.SiblingSelector;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class DirectAdjacentCssSelectorTest {
+
+    DirectAdjacentCssSelector directAdjacentCssSelector = new DirectAdjacentCssSelector();
+
+    @Test
+    public void testToXPath() throws Exception {
+        // given
+        ParsedSelector<SelectorList> parsedSelector = SelectorParser.parseSelector("a + b");
+        SiblingSelector siblingSelector = (SiblingSelector) parsedSelector.getSelector().item(0);
+        // when
+        XPathComponent xPathComponent = directAdjacentCssSelector.toXPath(parsedSelector.getStringMap(), siblingSelector);
+        // then
+        String xPath = xPathComponent.toXPath();
+        assertThat(xPath, is("(.//*[self::a]/following-sibling::b[position() = 1])"));
+    }
+
+}
---
+++ src/test/java/io/github/seleniumquery/by/css/combinators/GeneralAdjacentCssSelectorTest.java
@@ -0,0 +1,29 @@
+package io.github.seleniumquery.by.css.combinators;
+
+import io.github.seleniumquery.by.preparser.ParsedSelector;
+import io.github.seleniumquery.by.preparser.SelectorParser;
+import io.github.seleniumquery.by.xpath.XPathComponent;
+import org.junit.Test;
+import org.w3c.css.sac.SelectorList;
+import org.w3c.css.sac.SiblingSelector;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class GeneralAdjacentCssSelectorTest {
+
+    GeneralAdjacentCssSelector generalAdjacentCssSelector = new GeneralAdjacentCssSelector();
+
+    @Test
+    public void testToXPath() throws Exception {
+        // given
+        ParsedSelector<SelectorList> parsedSelector = SelectorParser.parseSelector("a ~ b");
+        SiblingSelector siblingSelector = (SiblingSelector) parsedSelector.getSelector().item(0);
+        // when
+        XPathComponent xPathComponent = generalAdjacentCssSelector.toXPath(parsedSelector.getStringMap(), siblingSelector);
+        // then
+        String xPath = xPathComponent.toXPath();
+        assertThat(xPath, is("(.//*[self::a]/following-sibling::b)"));
+    }
+
+}
