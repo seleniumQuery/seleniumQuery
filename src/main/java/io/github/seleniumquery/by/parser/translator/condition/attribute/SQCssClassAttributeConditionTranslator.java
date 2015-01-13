96854e060624889a71dd07c43fd7dbcaebcebf84
acdcjunior <acdcjunior@gmail.com>
2015-01-13 18:00:08 -0200

    Removed some unused parameters

--- src/main/java/io/github/seleniumquery/by/parser/translator/condition/attribute/SQCssClassAttributeConditionTranslator.java
+++ src/main/java/io/github/seleniumquery/by/parser/translator/condition/attribute/SQCssClassAttributeConditionTranslator.java
@@ -3,9 +3,6 @@ package io.github.seleniumquery.by.parser.translator.condition.attribute;
 import io.github.seleniumquery.by.parser.parsetree.condition.attribute.SQCssClassAttributeCondition;
 import org.unbescape.css.CssEscape;
 import org.w3c.css.sac.AttributeCondition;
-import org.w3c.css.sac.SimpleSelector;
-
-import java.util.Map;
 
 /**
  * .class
@@ -15,7 +12,7 @@ import java.util.Map;
  */
 public class SQCssClassAttributeConditionTranslator {
 
-	public SQCssClassAttributeCondition translate(SimpleSelector simpleSelector, Map<String, String> stringMap, AttributeCondition attributeCondition) {
+	public SQCssClassAttributeCondition translate(AttributeCondition attributeCondition) {
 		String wantedClassName = attributeCondition.getValue();
 		String unescapedClassName = CssEscape.unescapeCss(wantedClassName);
 		return new SQCssClassAttributeCondition(unescapedClassName);
