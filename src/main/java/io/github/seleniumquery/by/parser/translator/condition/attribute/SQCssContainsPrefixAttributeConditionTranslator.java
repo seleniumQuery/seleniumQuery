da271804a7527e09d3392e7610027fc66c4f2c12
acdcjunior <acdcjunior@gmail.com>
2015-01-13 18:00:08 -0200

    Removed some unused parameters

--- src/main/java/io/github/seleniumquery/by/parser/translator/condition/attribute/SQCssContainsPrefixAttributeConditionTranslator.java
+++ src/main/java/io/github/seleniumquery/by/parser/translator/condition/attribute/SQCssContainsPrefixAttributeConditionTranslator.java
@@ -1,28 +1,17 @@
 package io.github.seleniumquery.by.parser.translator.condition.attribute;
 
-import io.github.seleniumquery.by.parser.parsetree.condition.SQCssCondition;
 import io.github.seleniumquery.by.parser.parsetree.condition.attribute.SQCssContainsPrefixAttributeCondition;
 import org.w3c.css.sac.AttributeCondition;
-import org.w3c.css.sac.SimpleSelector;
-
-import java.util.Map;
 
 /**
- * [languages|="fr"]
- *
- * see {@link org.w3c.css.sac.Condition#SAC_BEGIN_HYPHEN_ATTRIBUTE_CONDITION}
- *
- * This condition checks if the value is in a hypen-separated list of values in a specified attribute. example:
- * [languages|="fr"]
- *
- * Case INsensitve
+ * [languages|="pt"]
  *
  * @author acdcjunior
  * @since 0.10.0
  */
 public class SQCssContainsPrefixAttributeConditionTranslator {
 
-	public SQCssContainsPrefixAttributeCondition translate(SimpleSelector simpleSelector, Map<String, String> stringMap, AttributeCondition attributeCondition) {
+	public SQCssContainsPrefixAttributeCondition translate(AttributeCondition attributeCondition) {
 		return new SQCssContainsPrefixAttributeCondition(attributeCondition.getLocalName(), attributeCondition.getValue());
 	}
 
--- src/main/java/io/github/seleniumquery/by/parser/translator/condition/attribute/SQCssContainsSubstringAttributeConditionTranslator.java
+++ src/main/java/io/github/seleniumquery/by/parser/translator/condition/attribute/SQCssContainsSubstringAttributeConditionTranslator.java
@@ -2,27 +2,16 @@ package io.github.seleniumquery.by.parser.translator.condition.attribute;
 
 import io.github.seleniumquery.by.parser.parsetree.condition.attribute.SQCssContainsSubstringAttributeCondition;
 import org.w3c.css.sac.AttributeCondition;
-import org.w3c.css.sac.SimpleSelector;
-
-import java.util.Map;
 
 /**
  * [attribute*=stringToContain]
- *
- * Currently it is (mistakenly?) mapped to the type {@link org.w3c.css.sac.Condition#SAC_ATTRIBUTE_CONDITION}.
- * The factory then inspects the actual type and redirects here.
- *
- * This selector is:
- * [attribute*=stringToContain]
- *
- * CASE INsensitive!
  *
  * @author acdcjunior
  * @since 0.10.0
  */
 public class SQCssContainsSubstringAttributeConditionTranslator {
 
-	public SQCssContainsSubstringAttributeCondition translate(SimpleSelector simpleSelector, Map<String, String> stringMap, AttributeCondition attributeCondition) {
+	public SQCssContainsSubstringAttributeCondition translate(AttributeCondition attributeCondition) {
 		return new SQCssContainsSubstringAttributeCondition(attributeCondition.getLocalName(), attributeCondition.getValue());
 	}
 
