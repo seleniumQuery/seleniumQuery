098ead64d2ae9f46937e954fd9f16e9ef64bba50
acdcjunior <acdcjunior@gmail.com>
2015-01-13 13:42:30 -0200

    Added tests and implemented SQCssClassAttributeConditionTranslator

--- src/main/java/io/github/seleniumquery/by/parser/parsetree/selector/SQCssTagNameSelector.java
+++ src/main/java/io/github/seleniumquery/by/parser/parsetree/selector/SQCssTagNameSelector.java
@@ -1,11 +1,15 @@
 package io.github.seleniumquery.by.parser.parsetree.selector;
 
-import io.github.seleniumquery.by.parser.parsetree.selector.SQCssSelector;
-
 public class SQCssTagNameSelector implements SQCssSelector {
 
-    public SQCssTagNameSelector(String tagName) {
+    private String tagName;
 
+    public SQCssTagNameSelector(String tagName) {
+        this.tagName = tagName;
+    }
+
+    public String getTagName() {
+        return tagName;
     }
 
 }
--- src/main/java/io/github/seleniumquery/by/parser/translator/condition/attribute/SQCssClassAttributeConditionTranslator.java
+++ src/main/java/io/github/seleniumquery/by/parser/translator/condition/attribute/SQCssClassAttributeConditionTranslator.java
@@ -1,7 +1,7 @@
 package io.github.seleniumquery.by.parser.translator.condition.attribute;
 
 import io.github.seleniumquery.by.parser.parsetree.condition.attribute.SQCssClassAttributeCondition;
-import org.apache.commons.lang3.StringEscapeUtils;
+import org.unbescape.css.CssEscape;
 import org.w3c.css.sac.AttributeCondition;
 import org.w3c.css.sac.SimpleSelector;
 
@@ -10,10 +10,6 @@ import java.util.Map;
 /**
  * .class
  *
- * see {@link org.w3c.css.sac.Condition#SAC_CLASS_CONDITION}
- *
- * This condition checks for a specified class. Example: .example
- *
  * @author acdcjunior
  * @since 0.10.0
  */
@@ -21,7 +17,7 @@ public class SQCssClassAttributeConditionTranslator {
 
 	public SQCssClassAttributeCondition translate(SimpleSelector simpleSelector, Map<String, String> stringMap, AttributeCondition attributeCondition) {
 		String wantedClassName = attributeCondition.getValue();
-		String unescapedClassName = StringEscapeUtils.unescapeJava(wantedClassName);
+		String unescapedClassName = CssEscape.unescapeCss(wantedClassName);
 		return new SQCssClassAttributeCondition(unescapedClassName);
 	}
 	
