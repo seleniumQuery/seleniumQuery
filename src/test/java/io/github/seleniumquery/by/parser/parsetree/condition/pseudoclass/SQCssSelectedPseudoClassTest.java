d2d7ddd191aa8d6abed0e206fc40550b07cff2da
acdcjunior <acdcjunior@gmail.com>
2015-01-15 18:38:46 -0200

    Added test for :selected translator

---
+++ src/test/java/io/github/seleniumquery/by/parser/parsetree/condition/pseudoclass/SQCssSelectedPseudoClassTest.java
@@ -0,0 +1,22 @@
+package io.github.seleniumquery.by.parser.parsetree.condition.pseudoclass;
+
+import org.junit.Test;
+
+import static io.github.seleniumquery.by.parser.translator.condition.attribute.TranslatorsTestUtils.parseFirstCssCondition;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.junit.Assert.assertThat;
+
+public class SQCssSelectedPseudoClassTest {
+
+    @Test
+    public void translate() {
+        // given
+        String selector = ":selected";
+        // when
+        SQCssSelectedPseudoClass cssCondition = parseFirstCssCondition(selector, SQCssSelectedPseudoClass.class);
+        // then
+        assertThat(cssCondition, is(notNullValue()));
+    }
+
+}
