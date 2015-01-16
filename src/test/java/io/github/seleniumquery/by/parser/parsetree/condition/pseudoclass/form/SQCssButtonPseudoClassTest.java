70c5df5637587a362fc2fa02c9f1e82ee47620da
acdcjunior <acdcjunior@gmail.com>
2015-01-16 18:08:50 -0200

    Moved PseudoClassTestUtils - it didnt make sense there anymore

--- src/test/java/io/github/seleniumquery/by/parser/parsetree/condition/pseudoclass/form/SQCssButtonPseudoClassTest.java
+++ src/test/java/io/github/seleniumquery/by/parser/parsetree/condition/pseudoclass/form/SQCssButtonPseudoClassTest.java
@@ -2,7 +2,7 @@ package io.github.seleniumquery.by.parser.parsetree.condition.pseudoclass.form;
 
 import org.junit.Test;
 
-import static io.github.seleniumquery.by.parser.parsetree.condition.pseudoclass.form.PseudoClassTestUtils.assertPseudo;
+import static io.github.seleniumquery.by.parser.parsetree.condition.pseudoclass.PseudoClassTestUtils.assertPseudo;
 
 public class SQCssButtonPseudoClassTest {
 
