4181ea741450c027b689c389168acaa054f0a711
acdcjunior <acdcjunior@gmail.com>
2015-01-16 20:11:17 -0200

    Implemented SQCssDescendantSelector

---
+++ src/test/java/io/github/seleniumquery/by/parser/translator/selector/combinator/SQCssDescendantSelectorTranslatorTest.java
@@ -0,0 +1,31 @@
+package io.github.seleniumquery.by.parser.translator.selector.combinator;
+
+import io.github.seleniumquery.by.parser.SQParseTreeBuilder;
+import io.github.seleniumquery.by.parser.parsetree.selector.SQCssSelector;
+import io.github.seleniumquery.by.parser.parsetree.selector.SQCssTagNameSelector;
+import io.github.seleniumquery.by.parser.parsetree.selector.combinator.SQCssDescendantSelector;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SQCssDescendantSelectorTranslatorTest {
+
+    @Test
+    public void translate() {
+        // given
+        SQCssSelector cssSelector = SQParseTreeBuilder.parse("a b").firstSelector();
+        assertThat(cssSelector, instanceOf(SQCssDescendantSelector.class));
+        // when
+        SQCssSelector ancestorSelector = ((SQCssDescendantSelector) cssSelector).getAncestorSelector();
+        SQCssSelector descendantSelector = ((SQCssDescendantSelector) cssSelector).getDescendantSelector();
+        // then
+        assertThat(ancestorSelector, instanceOf(SQCssTagNameSelector.class));
+        assertThat(((SQCssTagNameSelector) ancestorSelector).getTagName(), is("a"));
+
+        assertThat(descendantSelector, instanceOf(SQCssTagNameSelector.class));
+        assertThat(((SQCssTagNameSelector) descendantSelector).getTagName(), is("b"));
+    }
+
+}
