c8a2943add28037ef5b716e0137603b3673d6f07
acdcjunior <acdcjunior@gmail.com>
2015-01-16 19:16:33 -0200

    Improved error message for unsupported pseudos

--- src/main/java/io/github/seleniumquery/by/css/pseudoclasses/UnsupportedPseudoClassException.java
+++ src/main/java/io/github/seleniumquery/by/css/pseudoclasses/UnsupportedPseudoClassException.java
@@ -7,7 +7,7 @@ import java.util.Map;
 public class UnsupportedPseudoClassException extends RuntimeException {
 	
 	public UnsupportedPseudoClassException(String pseudoClass) {
-		super("The pseudo-class \""+pseudoClass+"\" is currently not supported.");
+		super("The pseudo-class \""+pseudoClass+"\" is not supported.");
 	}
 
 	public UnsupportedPseudoClassException(Map<String, String> stringMap, SimpleSelector selectorUpToThisPoint, String pseudoClassValue) {
