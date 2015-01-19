35366910841677730a5e6f4a839ed24e885eba52
acdcjunior <acdcjunior@gmail.com>
2015-01-19 16:36:33 -0200

    Added test for SQCssConditionalSelectorTranslator

---
+++ src/test/java/io/github/seleniumquery/by/parser/translator/selector/SQCssConditionalSelectorTranslatorTest.java
@@ -0,0 +1,33 @@
+package io.github.seleniumquery.by.parser.translator.selector;
+
+import io.github.seleniumquery.by.parser.SQParseTreeBuilder;
+import io.github.seleniumquery.by.parser.parsetree.condition.SQCssCondition;
+import io.github.seleniumquery.by.parser.parsetree.condition.attribute.SQCssClassAttributeCondition;
+import io.github.seleniumquery.by.parser.parsetree.selector.SQCssConditionalSelector;
+import io.github.seleniumquery.by.parser.parsetree.selector.SQCssSelector;
+import io.github.seleniumquery.by.parser.parsetree.selector.SQCssTagNameSelector;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+public class SQCssConditionalSelectorTranslatorTest {
+
+    @Test
+    public void translate() {
+        // given
+        SQCssSelector cssSelector = SQParseTreeBuilder.parse("a.condition").firstSelector();
+        assertThat(cssSelector, instanceOf(SQCssConditionalSelector.class));
+        // when
+        SQCssSelector sqCssSelector = ((SQCssConditionalSelector) cssSelector).getSqCssSelector();
+        SQCssCondition sqCssCondition = ((SQCssConditionalSelector) cssSelector).getSqCssCondition();
+        // then
+        assertThat(sqCssSelector, instanceOf(SQCssTagNameSelector.class));
+        assertThat(((SQCssTagNameSelector) sqCssSelector).getTagName(), is("a"));
+
+        assertThat(sqCssCondition, instanceOf(SQCssClassAttributeCondition.class));
+        assertThat(((SQCssClassAttributeCondition) sqCssCondition).getClassName(), is("condition"));
+    }
+
+}
