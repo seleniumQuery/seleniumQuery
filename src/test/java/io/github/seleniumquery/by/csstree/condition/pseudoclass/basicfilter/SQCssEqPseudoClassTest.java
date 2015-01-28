dc1289a0536ead9cbbe54e305fdf5a575d19dec1
acdcjunior <acdcjunior@gmail.com>
2015-01-28 18:41:38 -0200

    by.parser.parsetree.condition.pseudoclass -> by.csstree.condition.pseudoclass

---
+++ src/test/java/io/github/seleniumquery/by/csstree/condition/pseudoclass/basicfilter/SQCssEqPseudoClassTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2015 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.github.seleniumquery.by.csstree.condition.pseudoclass.basicfilter;
+
+import org.junit.Test;
+
+import static io.github.seleniumquery.by.csstree.condition.pseudoclass.PseudoClassTestUtils.assertFunctionalPseudo;
+
+public class SQCssEqPseudoClassTest {
+
+    @Test
+    public void translate() {
+        assertFunctionalPseudo(":eq", SQCssEqPseudoClass.class);
+    }
+
+}
