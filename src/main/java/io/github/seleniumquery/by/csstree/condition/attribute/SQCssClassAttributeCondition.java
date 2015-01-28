461f6c0fc7a7222d1630f1775351e1ed79fcffc6
acdcjunior <acdcjunior@gmail.com>
2015-01-28 18:53:00 -0200

    Added toSQLocator for SQCssIdAttributeCondition

--- src/main/java/io/github/seleniumquery/by/csstree/condition/attribute/SQCssClassAttributeCondition.java
+++ src/main/java/io/github/seleniumquery/by/csstree/condition/attribute/SQCssClassAttributeCondition.java
@@ -1,6 +1,23 @@
+/*
+ * Copyright (c) 2015 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.seleniumquery.by.csstree.condition.attribute;
 
 import io.github.seleniumquery.by.csstree.condition.SQCssCondition;
+import io.github.seleniumquery.by.csstree.condition.SQCssConditionImplementedLocators;
 import io.github.seleniumquery.by.locator.SQLocator;
 import io.github.seleniumquery.by.locator.SQLocatorUtils;
 
@@ -10,7 +27,7 @@ import io.github.seleniumquery.by.locator.SQLocatorUtils;
  * @author acdcjunior
  * @since 0.10.0
  */
-public class SQCssClassAttributeCondition implements SQCssCondition {
+public class SQCssClassAttributeCondition implements SQCssCondition, SQCssConditionImplementedLocators {
 
     private String unescapedClassName;
 
@@ -22,6 +39,7 @@ public class SQCssClassAttributeCondition implements SQCssCondition {
         return unescapedClassName;
     }
 
+    @Override
     public SQLocator toSQLocator(SQLocator leftLocator) {
         String newCssSelector = SQLocatorUtils.cssMerge(leftLocator.getCssSelector(), toCSS());
         String newXPathExpression = SQLocatorUtils.conditionalSimpleXPathMerge(leftLocator.getXPathExpression(), toXPath());
--- src/test/java/io/github/seleniumquery/by/csstree/condition/attribute/SQCssIdAttributeConditionTest.java
+++ src/test/java/io/github/seleniumquery/by/csstree/condition/attribute/SQCssIdAttributeConditionTest.java
@@ -14,6 +14,22 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright (c) 2015 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.seleniumquery.by.csstree.condition.attribute;
 
 import io.github.seleniumquery.by.filter.ElementFilter;
@@ -25,18 +41,18 @@ import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
 import static org.junit.Assert.assertThat;
 
-public class SQCssClassAttributeConditionTest {
+public class SQCssIdAttributeConditionTest {
 
     @Test
     public void toSQLocator() {
         // given
-        SQCssClassAttributeCondition classAttributeCondition = new SQCssClassAttributeCondition("clazz");
+        SQCssIdAttributeCondition idAttributeCondition = new SQCssIdAttributeCondition("idz");
         SQLocator previous = SQLocatorUtilsTest.TAG_ASTERISK;
         // when
-        SQLocator locator = classAttributeCondition.toSQLocator(previous);
+        SQLocator locator = idAttributeCondition.toSQLocator(previous);
         // then
-        assertThat(locator.getCssSelector(), is(".clazz"));
-        assertThat(locator.getXPathExpression(), is(".//*[contains(concat(' ', normalize-space(@class), ' '), ' clazz ')]"));
+        assertThat(locator.getCssSelector(), is("#idz"));
+        assertThat(locator.getXPathExpression(), is(".//*[@id = 'idz']"));
         assertThat(locator.canPureCss(), is(true));
         assertThat(locator.canPureXPath(), is(true));
         assertThat(locator.getElementFilterList().getElementFilters(), contains(ElementFilter.FILTER_NOTHING));
