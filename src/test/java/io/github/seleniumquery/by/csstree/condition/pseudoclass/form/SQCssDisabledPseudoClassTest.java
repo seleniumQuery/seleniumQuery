bab57d4d6290aa0dbe271ead978188f9e924013a
acdcjunior <acdcjunior@gmail.com>
2015-01-30 19:18:35 -0200

    Functional pseudo classes do not require an arg when constructing

--- src/test/java/io/github/seleniumquery/by/csstree/condition/pseudoclass/form/SQCssDisabledPseudoClassTest.java
+++ src/test/java/io/github/seleniumquery/by/csstree/condition/pseudoclass/form/SQCssDisabledPseudoClassTest.java
@@ -30,6 +30,22 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright (c) 2015 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.seleniumquery.by.csstree.condition.pseudoclass.form;
 
 import io.github.seleniumquery.by.filter.ElementFilter;
@@ -52,7 +68,7 @@ public class SQCssDisabledPseudoClassTest {
     @Test
     public void toSQLocator__when_driver_has_native_support() {
         // given
-        SQCssDisabledPseudoClass disabledPseudoClass = new SQCssDisabledPseudoClass(null);
+        SQCssDisabledPseudoClass disabledPseudoClass = new SQCssDisabledPseudoClass();
         SQLocator previous = SQLocatorUtilsTest.tagAsterisk(SQLocatorUtilsTest.createMockDriverWithNativeSupportFor(":disabled"));
         // when
         SQLocator locator = disabledPseudoClass.toSQLocator(previous);
@@ -69,7 +85,7 @@ public class SQCssDisabledPseudoClassTest {
     @Test
     public void toSQLocator__when_driver_does_NOT_have_native_support() {
         // given
-        SQCssDisabledPseudoClass disabledPseudoClass = new SQCssDisabledPseudoClass(null);
+        SQCssDisabledPseudoClass disabledPseudoClass = new SQCssDisabledPseudoClass();
         SQLocator previous = SQLocatorUtilsTest.tagAsterisk(SQLocatorUtilsTest.createMockDriverWithoutNativeSupportFor(":disabled"));
         // when
         SQLocator locator = disabledPseudoClass.toSQLocator(previous);
--- src/test/java/io/github/seleniumquery/by/csstree/condition/pseudoclass/visibility/SQCssHiddenPseudoClassTest.java
+++ src/test/java/io/github/seleniumquery/by/csstree/condition/pseudoclass/visibility/SQCssHiddenPseudoClassTest.java
@@ -30,6 +30,22 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright (c) 2015 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.seleniumquery.by.csstree.condition.pseudoclass.visibility;
 
 import io.github.seleniumquery.by.css.pseudoclasses.HiddenPseudoClass;
@@ -52,7 +68,7 @@ public class SQCssHiddenPseudoClassTest {
     @Test
     public void toSQLocator__no_browser_has_native_support() {
         // given
-        SQCssHiddenPseudoClass hiddenPseudoClass = new SQCssHiddenPseudoClass(null);
+        SQCssHiddenPseudoClass hiddenPseudoClass = new SQCssHiddenPseudoClass();
         SQLocator previous = SQLocatorUtilsTest.tagAsterisk(SQLocatorUtilsTest.createMockDriverWithoutNativeSupportFor(":hidden"));
         // when
         SQLocator locator = hiddenPseudoClass.toSQLocator(previous);
