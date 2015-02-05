8452f562f997e51a03e6aae0388a44dc09239351
acdcjunior <acdcjunior@gmail.com>
2015-02-05 18:48:33 -0200

    Changed exception word and removed duplicate license notice

--- src/main/java/io/github/seleniumquery/by/csstree/selector/combinator/SQCssCombinationSelector.java
+++ src/main/java/io/github/seleniumquery/by/csstree/selector/combinator/SQCssCombinationSelector.java
@@ -30,22 +30,6 @@
  * limitations under the License.
  */
 
-/*
- * Copyright (c) 2015 seleniumQuery authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.github.seleniumquery.by.csstree.selector.combinator;
 
 import io.github.seleniumquery.by.csstree.selector.SQCssSelector;
@@ -84,7 +68,7 @@ abstract class SQCssCombinationSelector implements SQCssSelector {
     @Override
     public SQLocator toSQLocator(SQLocator leftLocator) {
         throw new UnsupportedOperationException("Due to the way the CSS Tree is created by the CSS Parser, this " +
-                "order of evaluation should never happen.");
+                "order of evaluation should never occurr.");
     }
 
 }
