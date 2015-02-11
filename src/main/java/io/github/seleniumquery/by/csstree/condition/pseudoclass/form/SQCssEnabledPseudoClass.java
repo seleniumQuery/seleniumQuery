8380ca76c34bc97324a7fdab4b8d4edd499e3bc9
acdcjunior <acdcjunior@gmail.com>
2015-02-11 18:50:10 -0200

    Code cleaning on :enabled and :disabled

--- src/main/java/io/github/seleniumquery/by/csstree/condition/pseudoclass/form/SQCssEnabledPseudoClass.java
+++ src/main/java/io/github/seleniumquery/by/csstree/condition/pseudoclass/form/SQCssEnabledPseudoClass.java
@@ -46,6 +46,22 @@
  * limitations under the License.
  */
 
+/*
+ * Copyright (c) 2015 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.seleniumquery.by.csstree.condition.pseudoclass.form;
 
 import io.github.seleniumquery.by.DriverVersionUtils;
@@ -63,20 +79,26 @@ public class SQCssEnabledPseudoClass extends SQCssPseudoClassCondition implement
 
     @Override
     public SQLocator toSQLocator(SQLocator leftLocator) {
-        String newXPathExpression = SQLocatorUtils.conditionalSimpleXPathMerge(leftLocator.getXPathExpression(), toXPath());
         if (!supportsDisabledNatively(leftLocator)) {
-            return SQLocatorFactory.createPureXPathOnly(leftLocator, newXPathExpression);
+            return SQLocatorFactory.createPureXPathOnly(leftLocator, mergeXPath(leftLocator));
         }
-        String newCssSelector = SQLocatorUtils.cssMerge(leftLocator.getCssSelector(), toCSS());
-        return new SQLocator(newCssSelector, newXPathExpression, leftLocator);
+        return new SQLocator(mergeCss(leftLocator), mergeXPath(leftLocator), leftLocator);
     }
 
     private boolean supportsDisabledNatively(SQLocator leftLocator) {
         return DriverVersionUtils.getInstance().hasNativeSupportForPseudo(leftLocator.getWebDriver(), ENABLED_PSEUDO);
     }
 
+    private String mergeCss(SQLocator leftLocator) {
+        return SQLocatorUtils.cssMerge(leftLocator.getCssSelector(), toCSS());
+    }
+
     private String toCSS() {
         return ENABLED_PSEUDO;
+    }
+
+    private String mergeXPath(SQLocator leftLocator) {
+        return SQLocatorUtils.conditionalSimpleXPathMerge(leftLocator.getXPathExpression(), toXPath());
     }
 
     private String toXPath() {
