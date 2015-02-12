eee43a03d15ef5db015f7a225637725033e1a8aa
acdcjunior <acdcjunior@gmail.com>
2015-02-12 14:02:31 -0200

    Refactored out (extracted) superclass for use in other selectors that are similar

--- src/main/java/io/github/seleniumquery/by/csstree/condition/pseudoclass/form/SQCssFilePseudoClass.java
+++ src/main/java/io/github/seleniumquery/by/csstree/condition/pseudoclass/form/SQCssFilePseudoClass.java
@@ -14,45 +14,14 @@
  * limitations under the License.
  */
 
-/*
- * Copyright (c) 2015 seleniumQuery authors
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *    http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 package io.github.seleniumquery.by.csstree.condition.pseudoclass.form;
 
-import io.github.seleniumquery.by.csstree.condition.SQCssConditionImplementedLocators;
-import io.github.seleniumquery.by.csstree.condition.pseudoclass.SQCssPseudoClassCondition;
-import io.github.seleniumquery.by.locator.SQLocator;
-import io.github.seleniumquery.by.locator.SQLocatorFactory;
-import io.github.seleniumquery.by.locator.SQLocatorUtils;
-
-public class SQCssFilePseudoClass extends SQCssPseudoClassCondition implements SQCssConditionImplementedLocators {
+public class SQCssFilePseudoClass extends SQCssInputTypeAttributePseudoClass {
 
     public static final String PSEUDO = "file";
 
-    @Override
-    public SQLocator toSQLocator(SQLocator leftLocator) {
-        return SQLocatorFactory.createPureXPathOnly(leftLocator, mergeXPath(leftLocator));
-    }
-
-    private String mergeXPath(SQLocator leftLocator) {
-        return SQLocatorUtils.conditionalSimpleXPathMerge(leftLocator.getXPathExpression(), toXPath());
-    }
-
-    private String toXPath() {
-        return "(self::input and @type = '" + PSEUDO + "')";
+    public SQCssFilePseudoClass() {
+        super(PSEUDO);
     }
 
 }
