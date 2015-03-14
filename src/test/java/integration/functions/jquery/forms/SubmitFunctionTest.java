f9490e1e3b7a72caabea6aba32d8fc76cd555ef3
acdcjunior <acdcjunior@gmail.com>
2015-03-14 17:32:43 -0300

    Fixed @Rule and license notice for .submit() function test files

--- src/test/java/integration/functions/jquery/forms/SubmitFunctionTest.java
+++ src/test/java/integration/functions/jquery/forms/SubmitFunctionTest.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2015 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package integration.functions.jquery.forms;
 
 import infrastructure.junitrule.JavaScriptOnly;
@@ -12,8 +28,7 @@ import static org.junit.Assert.assertThat;
 
 public class SubmitFunctionTest {
 
-    @ClassRule public static SetUpAndTearDownDriver setUpAndTearDownDriverRule = new SetUpAndTearDownDriver();
-    @Rule public SetUpAndTearDownDriver setUpAndTearDownDriverRuleInstance = setUpAndTearDownDriverRule;
+    @ClassRule @Rule public static SetUpAndTearDownDriver setUpAndTearDownDriverRule = new SetUpAndTearDownDriver();
 
     @Test @JavaScriptOnly
     public void submit_function() {
