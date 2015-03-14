01196f7af57ab8b4ce40dd82cb1723035630cab7
acdcjunior <acdcjunior@gmail.com>
2015-03-14 16:36:33 -0300

    Added <head> for test file + copyright notice + @Rule/@ClassRule  join

--- src/test/java/integration/functions/FindWithPseudoClassTest.java
+++ src/test/java/integration/functions/FindWithPseudoClassTest.java
@@ -1,3 +1,19 @@
+/*
+Copyright (c) 2015 seleniumQuery authors
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
 package integration.functions;
 
 import static io.github.seleniumquery.SeleniumQuery.$;
@@ -5,13 +21,13 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 import infrastructure.junitrule.SetUpAndTearDownDriver;
 
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 
 public class FindWithPseudoClassTest {
 
-	@Rule
-	public SetUpAndTearDownDriver setUpAndTearDownDriverRule = new SetUpAndTearDownDriver(getClass());
+	@ClassRule @Rule public static SetUpAndTearDownDriver setUpAndTearDownDriverRule = new SetUpAndTearDownDriver();
 	
     @Test
     public void find_function_with_pseudo_optionChecked() {
