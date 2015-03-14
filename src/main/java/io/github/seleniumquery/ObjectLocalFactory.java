5b229e7da59acb631c6662f6af98cd66b49bd944
acdcjunior <acdcjunior@gmail.com>
2015-03-14 17:33:54 -0300

    Fixed license notice for ObjectLocalFactory class file

--- src/main/java/io/github/seleniumquery/ObjectLocalFactory.java
+++ src/main/java/io/github/seleniumquery/ObjectLocalFactory.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2015 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.seleniumquery;
 
 import org.openqa.selenium.WebDriver;
@@ -12,7 +28,6 @@ import static java.util.Arrays.asList;
  * constructors in that class have protected visibility (we don't want them public).
  *
  * @author acdcjunior
- *
  * @since 0.9.0
  */
 public class ObjectLocalFactory {
