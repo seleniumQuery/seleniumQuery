3ef24230f92ae88f6a7dd15e715e7ab809386e8f
acdcjunior <acdcjunior@gmail.com>
2015-03-14 23:51:22 -0300

    Fixed javadoc and license notice for FirefoxDriverBuilder class file

--- src/main/java/io/github/seleniumquery/browser/driver/builders/FirefoxDriverBuilder.java
+++ src/main/java/io/github/seleniumquery/browser/driver/builders/FirefoxDriverBuilder.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2015 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.seleniumquery.browser.driver.builders;
 
 import io.github.seleniumquery.browser.driver.DriverBuilder;
@@ -7,10 +23,9 @@ import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 /**
- * Builds FirefoxDriver instances for SeleniumQueryDriver.
+ * Builds {@link FirefoxDriver} instances for SeleniumQueryDriver.
  *
  * @author acdcjunior
- *
  * @since 0.9.0
  */
 public class FirefoxDriverBuilder extends DriverBuilder<FirefoxDriverBuilder> {
@@ -31,7 +46,7 @@ public class FirefoxDriverBuilder extends DriverBuilder<FirefoxDriverBuilder> {
     }
 
     /**
-     * Sets specific {@link org.openqa.selenium.firefox.FirefoxProfile} to be used in the {@link org.openqa.selenium.firefox.FirefoxDriver}.
+     * Sets specific {@link FirefoxProfile} to be used in the {@link FirefoxDriver}.
      *
      * @param firefoxProfile Profile to be used.
      * @return A self reference, allowing further configuration.
