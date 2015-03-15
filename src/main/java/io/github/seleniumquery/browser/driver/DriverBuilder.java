6183df5eb53bbf0f0c0a42a3d5b15e291e7f85b2
acdcjunior <acdcjunior@gmail.com>
2015-03-14 23:49:12 -0300

    Fixed javadoc and license notice for DriverBuilder class file

--- src/main/java/io/github/seleniumquery/browser/driver/DriverBuilder.java
+++ src/main/java/io/github/seleniumquery/browser/driver/DriverBuilder.java
@@ -1,13 +1,28 @@
+/*
+ * Copyright (c) 2015 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.seleniumquery.browser.driver;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 /**
- * Builds WebDriver instances for SeleniumQueryDriver.
+ * Builds {@link WebDriver} instances for SeleniumQueryDriver.
  *
  * @author acdcjunior
- *
  * @since 0.9.0
  */
 public abstract class DriverBuilder<T extends DriverBuilder<T>> {
