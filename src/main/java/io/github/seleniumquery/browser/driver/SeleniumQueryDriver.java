c5040d2d90ee7457253cd3d518975c389b3717f1
acdcjunior <acdcjunior@gmail.com>
2015-03-14 23:48:30 -0300

    Fixed javadoc and license notice for SeleniumQueryDriver class file

--- src/main/java/io/github/seleniumquery/browser/driver/SeleniumQueryDriver.java
+++ src/main/java/io/github/seleniumquery/browser/driver/SeleniumQueryDriver.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2015 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.seleniumquery.browser.driver;
 
 import io.github.seleniumquery.SeleniumQueryConfig;
@@ -9,11 +25,10 @@ import org.openqa.selenium.WebDriver;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Represents and manages the {@link org.openqa.selenium.WebDriver} instance used a specific
+ * Represents and manages the {@link WebDriver} instance used a specific
  * {@link io.github.seleniumquery.SeleniumQueryBrowser}.
  *
  * @author acdcjunior
- *
  * @since 0.9.0
  */
 public class SeleniumQueryDriver {
