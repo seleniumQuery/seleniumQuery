d9cd6151652f65c8349f13d256a4dd3cbd18ea9e
acdcjunior <acdcjunior@gmail.com>
2015-03-22 18:14:22 -0300

    from template method to strategy: refactored :submit

--- src/main/java/io/github/seleniumquery/by/csstree/condition/pseudoclass/form/SQCssSubmitPseudoClass.java
+++ src/main/java/io/github/seleniumquery/by/csstree/condition/pseudoclass/form/SQCssSubmitPseudoClass.java
@@ -16,17 +16,20 @@
 
 package io.github.seleniumquery.by.csstree.condition.pseudoclass.form;
 
+import io.github.seleniumquery.by.csstree.condition.pseudoclass.SQCssPseudoClassCondition;
 import io.github.seleniumquery.by.csstree.condition.pseudoclass.SQCssPseudoNeverNativelySupported;
 import io.github.seleniumquery.by.locator.SQLocatorXPath;
 
 import static io.github.seleniumquery.by.css.attributes.AttributeEvaluatorUtils.TYPE_ATTR_LC_VAL;
 
 /**
+ * :submit
+ * https://api.jquery.com/submit-selector/
  *
  * @author acdcjunior
  * @since 0.10.0
  */
-public class SQCssSubmitPseudoClass extends SQCssPseudoNeverNativelySupported {
+public class SQCssSubmitPseudoClass extends SQCssPseudoClassCondition {
 
     public static final String PSEUDO = "submit";
     public static final String SUBMIT_XPATH_EXPRESSION = "(" +
@@ -35,9 +38,16 @@ public class SQCssSubmitPseudoClass extends SQCssPseudoNeverNativelySupported {
             "(self::button and (" + TYPE_ATTR_LC_VAL + " = 'submit' or not(@type)))" +
         ")";
 
+    public SQCssPseudoNeverNativelySupported submitPseudoClassLocatorGenerationStrategy = new SQCssPseudoNeverNativelySupported() {
+        @Override
+        public SQLocatorXPath toXPath() {
+            return SQLocatorXPath.pureXPath(SUBMIT_XPATH_EXPRESSION);
+        }
+    };
+
     @Override
-    public SQLocatorXPath toXPath() {
-        return SQLocatorXPath.pureXPath(SUBMIT_XPATH_EXPRESSION);
+    public SQCssPseudoNeverNativelySupported getSQCssLocatorGenerationStrategy() {
+        return submitPseudoClassLocatorGenerationStrategy;
     }
 
 }
