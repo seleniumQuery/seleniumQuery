a2851ef5b9427ffbba1f7b534305739b3ba3ed45
acdcjunior <acdcjunior@gmail.com>
2015-03-22 18:39:39 -0300

    Added tests for :odd

--- src/test/java/io/github/seleniumquery/by/csstree/condition/pseudoclass/basicfilter/SQCssOddPseudoClassTest.java
+++ src/test/java/io/github/seleniumquery/by/csstree/condition/pseudoclass/basicfilter/SQCssOddPseudoClassTest.java
@@ -18,13 +18,27 @@ package io.github.seleniumquery.by.csstree.condition.pseudoclass.basicfilter;
 
 import org.junit.Test;
 
+import static io.github.seleniumquery.by.csstree.condition.pseudoclass.PseudoClassAssertLocatorUtils.assertPseudoClassOnlySupportsPureXPathRegardlessOfNativeSupport;
 import static io.github.seleniumquery.by.csstree.condition.pseudoclass.PseudoClassTestUtils.assertPseudo;
 
 public class SQCssOddPseudoClassTest {
 
+    public static final String ODD_PSEUDO = ":odd";
+    public static final String ODD_XPATH_EXPRESSION = ".//*[(position() mod 2) = 0]";
+
     @Test
     public void translate() {
-        assertPseudo(":odd", SQCssOddPseudoClass.class);
+        assertPseudo(ODD_PSEUDO, SQCssOddPseudoClass.class);
     }
+
+    @Test
+    public void toSQLocator__when_driver_does_NOT_have_native_support() {
+        assertPseudoClassOnlySupportsPureXPathRegardlessOfNativeSupport(
+                new SQCssOddPseudoClass(),
+                ODD_PSEUDO,
+                ODD_XPATH_EXPRESSION
+        );
+    }
+
 
 }
