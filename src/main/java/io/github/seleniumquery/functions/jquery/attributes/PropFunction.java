11fe1f51b28adfd11212676c510fbdf4e227a869
acdcjunior <acdcjunior@gmail.com>
2015-03-21 22:03:44 -0300

    Added license notice and javadoc header for PropFunction class

--- src/main/java/io/github/seleniumquery/functions/jquery/attributes/PropFunction.java
+++ src/main/java/io/github/seleniumquery/functions/jquery/attributes/PropFunction.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2015 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.seleniumquery.functions.jquery.attributes;
 
 import java.util.List;
@@ -7,6 +23,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import io.github.seleniumquery.SeleniumQueryObject;
 
+/**
+ * $("selector").prop("property-name");
+ * $("selector").prop("property-name", new-property-value);
+ *
+ * @author acdcjunior
+ * @since 0.9.0
+ */
 public class PropFunction {
 	
 	public static <T> T prop(SeleniumQueryObject seleniumQueryObject, List<WebElement> elements, String propertyName) {
