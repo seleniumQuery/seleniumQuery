7d7fad17027b76139a1fb91e326d537d6e4bd703
acdcjunior <acdcjunior@gmail.com>
2015-03-22 18:26:57 -0300

    Added tests for :first

--- src/test/java/io/github/seleniumquery/by/csstree/condition/pseudoclass/basicfilter/SQCssFirstPseudoClassTest.java
+++ src/test/java/io/github/seleniumquery/by/csstree/condition/pseudoclass/basicfilter/SQCssFirstPseudoClassTest.java
@@ -18,13 +18,26 @@ package io.github.seleniumquery.by.csstree.condition.pseudoclass.basicfilter;
 
 import org.junit.Test;
 
+import static io.github.seleniumquery.by.csstree.condition.pseudoclass.PseudoClassAssertLocatorUtils.assertPseudoClassOnlySupportsPureXPathRegardlessOfNativeSupport;
 import static io.github.seleniumquery.by.csstree.condition.pseudoclass.PseudoClassTestUtils.assertPseudo;
 
 public class SQCssFirstPseudoClassTest {
 
+    public static final String FIRST_PSEUDO = ":first";
+    public static final String FIRST_XPATH_EXPRESSION = ".//*[position() = 1]";
+
     @Test
     public void translate() {
-        assertPseudo(":first", SQCssFirstPseudoClass.class);
+        assertPseudo(FIRST_PSEUDO, SQCssFirstPseudoClass.class);
+    }
+
+    @Test
+    public void toSQLocator__when_driver_does_NOT_have_native_support() {
+        assertPseudoClassOnlySupportsPureXPathRegardlessOfNativeSupport(
+                new SQCssFirstPseudoClass(),
+                FIRST_PSEUDO,
+                FIRST_XPATH_EXPRESSION
+        );
     }
 
 }
