fd03359538d229cac241d48ca0df3ffd1ed6e6c1
acdcjunior <acdcjunior@gmail.com>
2015-03-28 18:12:39 -0300

    Added tests for :nth-child(<invalid arg>)

--- src/main/java/io/github/seleniumquery/by/csstree/condition/pseudoclass/childfilter/SQCssNthChildPseudoClass.java
+++ src/main/java/io/github/seleniumquery/by/csstree/condition/pseudoclass/childfilter/SQCssNthChildPseudoClass.java
@@ -17,15 +17,63 @@
 package io.github.seleniumquery.by.csstree.condition.pseudoclass.childfilter;
 
 import io.github.seleniumquery.by.css.pseudoclasses.PseudoClassSelector;
-import io.github.seleniumquery.by.csstree.condition.SQCssConditionImplementedNotYet;
 import io.github.seleniumquery.by.csstree.condition.pseudoclass.SQCssFunctionalPseudoClassCondition;
+import io.github.seleniumquery.by.csstree.condition.pseudoclass.locatorgenerationstrategy.MaybeNativelySupportedPseudoClass;
+import io.github.seleniumquery.by.locator.CSSLocator;
+import io.github.seleniumquery.by.locator.XPathLocator;
+import org.openqa.selenium.InvalidSelectorException;
 
-public class SQCssNthChildPseudoClass extends SQCssFunctionalPseudoClassCondition implements SQCssConditionImplementedNotYet {
+public class SQCssNthChildPseudoClass extends SQCssFunctionalPseudoClassCondition {
 
     public static final String PSEUDO = "nth-child";
 
+    public MaybeNativelySupportedPseudoClass nthChildPseudoClassLocatorGenerationStrategy = new MaybeNativelySupportedPseudoClass() {
+        @Override
+        public String pseudoClassForCSSNativeSupportCheck() {
+            return ":"+PSEUDO+"(1)";
+        }
+        @Override
+        public CSSLocator toCssWhenNativelySupported() {
+            getNthChildArgument();
+            return new CSSLocator(PSEUDO);
+        }
+        @Override
+        public XPathLocator toXPath() {
+            getNthChildArgument();
+            return null;
+        }
+    };
+
     public SQCssNthChildPseudoClass(PseudoClassSelector pseudoClassSelector) {
         super(pseudoClassSelector);
+    }
+
+    @Override
+    public MaybeNativelySupportedPseudoClass getSQCssLocatorGenerationStrategy() {
+        return nthChildPseudoClassLocatorGenerationStrategy;
+    }
+
+    private static class NthChildArgument {
+        public NthChildArgument(String argument) {
+            String reason = String.format("The :%s() pseudo-class requires an integer as argument but got: \"%s\".",
+                    PSEUDO, argument);
+            throw new InvalidSelectorException(reason);
+        }
+//        static final NthChildArgument ODD = new NthChildArgument(2,1);
+//        static final NthChildArgument EVEN = new NthChildArgument(2,0);
+//        final int a;
+//        final int b;
+//        NthChildArgument(int b) {
+//            this(0, b);
+//        }
+//        NthChildArgument(int a, int b) {
+//            this.a = a;
+//            this.b = b;
+//        }
+    }
+
+    private NthChildArgument getNthChildArgument() {
+        return new NthChildArgument(super.getArgument());
     }
 
 }
