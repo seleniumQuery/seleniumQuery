9071ac5656cae8d2955ae9caa5962f85488d9d5d
acdcjunior <acdcjunior@gmail.com>
2015-04-11 20:23:04 -0300

    Documented some expected XPath for some :nth-last-child() selectors

--- src/test/java/io/github/seleniumquery/by/csstree/condition/pseudoclass/childfilter/SQCssNthLastChildPseudoClassTest.java
+++ src/test/java/io/github/seleniumquery/by/csstree/condition/pseudoclass/childfilter/SQCssNthLastChildPseudoClassTest.java
@@ -27,4 +27,12 @@ public class SQCssNthLastChildPseudoClassTest {
         assertFunctionalPseudo(":nth-last-child", SQCssNthLastChildPseudoClass.class);
     }
 
+    // ":nth-last-child(2)"     -> "//*[last()+1- position() = 2]"
+    // ":nth-last-child(3)"     -> "//*[last()+1- position() = 3]"
+    // ":nth-last-child(5n)"    -> "//*[(last()+1- position() - 0) mod 5 = 0 and last()+1- position() >= 0]"
+    // ":nth-last-child(-5n)"   -> "//*[(last()+1- position() - 0) mod -5 = 0 and last()+1- position() <= 0]"
+    // ":nth-last-child(2n+3)"  -> "//*[(last()+1- position() - 3) mod 2 = 0 and last()+1- position() >= 3]"
+    // ":nth-last-child(2n-3)"  -> "//*[(last()+1- position() - -3) mod 2 = 0 and last()+1- position() >= -3]"
+    // ":nth-last-child(-2n+3)" -> "//*[(last()+1- position() - 3) mod -2 = 0 and last()+1- position() <= 3]"
+
 }
