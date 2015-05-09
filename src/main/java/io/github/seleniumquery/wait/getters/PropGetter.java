f52d0e9cacf701b8df497cde99aae720d49a1834
acdcjunior <acdcjunior@gmail.com>
2015-05-09 16:13:01 -0300

    #77 - removed this.element argument for .prop()

--- src/main/java/io/github/seleniumquery/wait/getters/PropGetter.java
+++ src/main/java/io/github/seleniumquery/wait/getters/PropGetter.java
@@ -1,10 +1,26 @@
+/*
+ * Copyright (c) 2015 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.seleniumquery.wait.getters;
 
-import java.util.List;
-
+import io.github.seleniumquery.functions.jquery.attributes.PropFunction;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import io.github.seleniumquery.functions.jquery.attributes.PropFunction;
+
+import java.util.List;
 
 public class PropGetter<T> implements Getter<T> {
 	
@@ -13,12 +29,12 @@ public class PropGetter<T> implements Getter<T> {
 	
 	@Override
 	public T get(WebDriver driver, List<WebElement> elements) {
-		return PropFunction.<T>prop(driver, elements, this.propertyName);
+		return PropFunction.prop(driver, elements, this.propertyName);
 	}
 	
 	@Override
 	public String toString() {
-		return "attr(\""+this.propertyName+"\")";
+		return "prop(\""+this.propertyName+"\")";
 	}
 	
 }
