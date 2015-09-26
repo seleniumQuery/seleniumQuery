1534ab1494b3afaef67c927c87014e576b2a372a
acdcjunior <acdcjunior@gmail.com>
2015-09-26 11:55:32 -0300

    Refactored attribute conditions so they reuse more code

--- src/main/java/io/github/seleniumquery/by/secondgen/csstree/condition/attribute/SQCssContainsPrefixAttributeCondition.java
+++ src/main/java/io/github/seleniumquery/by/secondgen/csstree/condition/attribute/SQCssContainsPrefixAttributeCondition.java
@@ -17,7 +17,6 @@
 package io.github.seleniumquery.by.secondgen.csstree.condition.attribute;
 
 import io.github.seleniumquery.by.common.AttributeEvaluatorUtils;
-import io.github.seleniumquery.by.secondgen.finder.CssFinder;
 import io.github.seleniumquery.utils.SelectorUtils;
 
 /**
@@ -32,8 +31,9 @@ public class SQCssContainsPrefixAttributeCondition extends SQCssAttributeConditi
         super(attributeName, wantedValue);
     }
 
-    protected CssFinder toCSS() {
-        return new CssFinder("[" + this.attributeName + "|='" + this.wantedValue + "']");
+    @Override
+    protected String symbol() {
+        return "|=";
     }
 
     protected String toXPath() {
