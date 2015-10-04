229c9a32f90ee5f3caa02326c1709cd347f85dbe
acdcjunior <acdcjunior@gmail.com>
2015-10-03 21:00:05 -0300

    HtmlUnit fixed the .attr() bug!

--- src/test/java/endtoend/functions/jquery/attributes/AttrFunctionTest.java
+++ src/test/java/endtoend/functions/jquery/attributes/AttrFunctionTest.java
@@ -1,17 +1,32 @@
+/*
+ * Copyright (c) 2015 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package endtoend.functions.jquery.attributes;
-
-import static io.github.seleniumquery.SeleniumQuery.$;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.nullValue;
-import static org.junit.Assert.assertThat;
-
-import testinfrastructure.junitrule.JavaScriptOnly;
-import testinfrastructure.junitrule.SetUpAndTearDownDriver;
 
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import testinfrastructure.junitrule.JavaScriptOnly;
+import testinfrastructure.junitrule.SetUpAndTearDownDriver;
+
+import static io.github.seleniumquery.SeleniumQuery.$;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
 
 public class AttrFunctionTest {
 
@@ -97,13 +112,8 @@ public class AttrFunctionTest {
     	assertThat($("#chk1").<Boolean>prop("checked"), is(false));
 
     	assertThat($("#chk1").attr("data-ball"), is("yo"));
-    	if ($.driver().get() instanceof HtmlUnitDriver) {
-    		// Properties follow the value of the attribute in HtmlUnitDriver. They shouldn't! This should be NULL!
-    		assertThat($("#chk1").<String>prop("data-ball"), is("yo"));
-    	} else {
-    		assertThat($("#chk1").<String>prop("data-ball"), is(nullValue()));
-    	}
-    	
+		assertThat($("#chk1").<String>prop("data-ball"), is(nullValue()));
+
     	$("#chk1").attr("data-ball", "abc");
     	assertThat($("#chk1").attr("data-ball"), is("abc"));
     }
