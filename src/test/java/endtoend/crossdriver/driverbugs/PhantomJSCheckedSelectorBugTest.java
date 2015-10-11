ff7dd8b4a626114f2f7e64568259433526feda1f
acdcjunior <acdcjunior@gmail.com>
2015-10-11 18:23:44 -0300

    The assumeTrue stuff is bugging me

--- src/test/java/endtoend/crossdriver/driverbugs/PhantomJSCheckedSelectorBugTest.java
+++ src/test/java/endtoend/crossdriver/driverbugs/PhantomJSCheckedSelectorBugTest.java
@@ -30,7 +30,6 @@ import java.util.List;
 import static io.github.seleniumquery.SeleniumQuery.$;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertThat;
-import static org.junit.Assume.assumeTrue;
 
 /**
  * This test exists so we can keep the workaround on :checked for PhantomJS driver.
@@ -43,16 +42,18 @@ public class PhantomJSCheckedSelectorBugTest {
 
     @Test
     public void confirm_CHECKED_brings_all_checked_elements_in_chrome() {
-        assumeTrue($.driver().get() instanceof ChromeDriver);
-        List<WebElement> checkedElements = $.driver().get().findElements(By.cssSelector(":checked"));
-        assertThat(checkedElements, hasSize(3));
+        if ($.driver().get() instanceof ChromeDriver) {
+            List<WebElement> checkedElements = $.driver().get().findElements(By.cssSelector(":checked"));
+            assertThat(checkedElements, hasSize(3));
+        }
     }
 
     @Test
     public void confirm_CHECKED_has_a_bug_and_does_NOT_bring_the_checked_OPTION_in_PhantomJS() {
-        assumeTrue($.driver().get() instanceof PhantomJSDriver);
-        List<WebElement> checkedElements = $.driver().get().findElements(By.cssSelector(":checked"));
-        assertThat(checkedElements, hasSize(2));
+        if ($.driver().get() instanceof PhantomJSDriver) {
+            List<WebElement> checkedElements = $.driver().get().findElements(By.cssSelector(":checked"));
+            assertThat(checkedElements, hasSize(2));
+        }
     }
 
 }
