37310c53001c5e633dd0ea19450dbceab9154cac
acdcjunior <acdcjunior@gmail.com>
2015-10-11 18:25:39 -0300

    Remove driver builder removed methods

--- src/test/java/testinfrastructure/junitrule/DriverInstantiator.java
+++ src/test/java/testinfrastructure/junitrule/DriverInstantiator.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2015 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package testinfrastructure.junitrule;
 
 import io.github.seleniumquery.browser.BrowserFunctions;
@@ -58,8 +74,6 @@ abstract class DriverInstantiator {
     };
     static DriverInstantiator HTMLUNIT_IE8_JS_ON   = new DriverInstantiator("HtmlUnit (IE8) - JS ON")   { @Override public void instantiateDriver(BrowserFunctions $) { $.driver().useHtmlUnit().emulatingInternetExplorer8(); } };
     static DriverInstantiator HTMLUNIT_IE8_JS_OFF  = new DriverInstantiator("HtmlUnit (IE8) - JS OFF")  { @Override public void instantiateDriver(BrowserFunctions $) { $.driver().useHtmlUnit().emulatingInternetExplorer8().withoutJavaScript(); } };
-    static DriverInstantiator HTMLUNIT_IE9_JS_ON   = new DriverInstantiator("HtmlUnit (IE9) - JS ON")   { @Override public void instantiateDriver(BrowserFunctions $) { $.driver().useHtmlUnit().emulatingInternetExplorer9(); } };
-    static DriverInstantiator HTMLUNIT_IE9_JS_OFF  = new DriverInstantiator("HtmlUnit (IE9) - JS OFF")  { @Override public void instantiateDriver(BrowserFunctions $) { $.driver().useHtmlUnit().emulatingInternetExplorer9().withoutJavaScript(); } };
     static DriverInstantiator HTMLUNIT_IE11_JS_ON  = new DriverInstantiator("HtmlUnit (IE11) - JS ON")  { @Override public void instantiateDriver(BrowserFunctions $) { $.driver().useHtmlUnit().emulatingInternetExplorer11(); } };
     static DriverInstantiator HTMLUNIT_IE11_JS_OFF = new DriverInstantiator("HtmlUnit (IE11) - JS OFF") { @Override public void instantiateDriver(BrowserFunctions $) { $.driver().useHtmlUnit().emulatingInternetExplorer11().withoutJavaScript(); } };
 
--- src/test/java/testinfrastructure/junitrule/DriverToRunTestsIn.java
+++ src/test/java/testinfrastructure/junitrule/DriverToRunTestsIn.java
@@ -1,10 +1,26 @@
+/*
+ * Copyright (c) 2015 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package testinfrastructure.junitrule;
 
 @SuppressWarnings("unused")
 public enum DriverToRunTestsIn {
 
 	ALL_DRIVERS_JS_ON_AND_OFF			(true,  true,  true,  true,  true,    true,  true),
-	ALL_DRIVERS_JS_ON_ONLY				(true,  true,  true,  true,  true,    true,  false),
+	ALL_DRIVERS_JS_ON_ONLY				(true,  true,  true,  false,  true,    true,  false),
 	HEADLESS_DRIVERS_JS_ON_AND_OFF      (false, false, false, true,  true,    true,  true),
 	HEADLESS_DRIVERS_JS_ON_ONLY         (false, false, false, true,  true,    true,  false),
 	NON_HEADLESS_DRIVERS_JS_ON_ONLY     (true,  true,  true,  false, false,   true,  false),
