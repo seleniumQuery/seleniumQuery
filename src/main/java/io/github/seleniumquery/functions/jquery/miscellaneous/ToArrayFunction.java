30f6c94e80cdd5c9b6d9a9085c8d6b060f8b05ff
acdcjunior <acdcjunior@gmail.com>
2015-12-27 23:23:57 -0200

    Removed elements argument from ToArrayFunction - see #77

--- src/main/java/io/github/seleniumquery/functions/jquery/miscellaneous/ToArrayFunction.java
+++ src/main/java/io/github/seleniumquery/functions/jquery/miscellaneous/ToArrayFunction.java
@@ -1,15 +1,30 @@
+/*
+ * Copyright (c) 2015 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.seleniumquery.functions.jquery.miscellaneous;
 
 import io.github.seleniumquery.SeleniumQueryObject;
+import org.openqa.selenium.WebElement;
 
 import java.util.List;
 
-import org.openqa.selenium.WebElement;
-
 public class ToArrayFunction {
 
-	public static WebElement[] toArray(@SuppressWarnings("unused") SeleniumQueryObject seleniumQueryObject,
-			List<WebElement> elements) {
+	public static WebElement[] toArray(SeleniumQueryObject seleniumQueryObject) {
+		List<WebElement> elements = seleniumQueryObject.get();
 		return elements.toArray(new WebElement[elements.size()]);
 	}
 
