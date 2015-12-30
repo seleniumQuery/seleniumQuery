32766815cafd1bcde6774a8f042b77d350ab03f6
acdcjunior <acdcjunior@gmail.com>
2015-12-29 23:22:32 -0200

    CI should run tests using PhantomJS and HtmlUnit with JS off as well

--- src/test/java/testinfrastructure/junitrule/SetUpAndTearDownDriver.java
+++ src/test/java/testinfrastructure/junitrule/SetUpAndTearDownDriver.java
@@ -35,7 +35,7 @@ import static io.github.seleniumquery.SeleniumQuery.$;
 @SuppressWarnings("unused")
 public class SetUpAndTearDownDriver implements TestRule {
 
-	public static final DriverToRunTestsIn driverToRunTestsIn = DriverToRunTestsIn.HTMLUNIT_CHROME_JS_ON_ONLY;
+	public static final DriverToRunTestsIn driverToRunTestsIn = DriverToRunTestsIn.HEADLESS_DRIVERS_JS_ON_AND_OFF;
 	private static final String NOT_SPECIFIED = null;
 
 	private final String testUrl;
