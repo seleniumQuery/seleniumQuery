1b38623989036b22d834f16d1d239b56628e7212
Antônio "acdc" Jr <acdcjunior@gmail.com>
2016-01-13 16:10:33 -0200

    Code is not picking up env property while at travis-ci

--- src/test/java/testinfrastructure/junitrule/SetUpAndTearDownDriver.java
+++ src/test/java/testinfrastructure/junitrule/SetUpAndTearDownDriver.java
@@ -28,10 +28,11 @@ public class SetUpAndTearDownDriver implements TestRule {
 	public static final DriverToRunTestsIn driverToRunTestsIn = whatDriversShouldTestsRun();
 
 	private static DriverToRunTestsIn whatDriversShouldTestsRun() {
-		if ("true".equals(System.getProperty("CI"))) {
+		System.out.println("@# Property is: "+System.getProperty("CI"));
+		//if ("true".equals(System.getProperty("CI"))) {
 			return DriverToRunTestsIn.HEADLESS_DRIVERS_JS_ON_AND_OFF;
-		}
-		return DriverToRunTestsIn.HTMLUNIT_CHROME_JS_ON_ONLY;
+		//}
+		//return DriverToRunTestsIn.HTMLUNIT_CHROME_JS_ON_ONLY;
 	}
 
 	private static final String NOT_SPECIFIED = null;
