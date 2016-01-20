ad89afefeae312df90054d587d9c76227c50e602
acdcjunior <acdcjunior@gmail.com>
2016-01-19 22:46:53 -0200

    #97 Done $("").filter(<PREDICATE>)

--- src/main/java/io/github/seleniumquery/functions/jquery/traversing/filtering/FilterFunction.java
+++ src/main/java/io/github/seleniumquery/functions/jquery/traversing/filtering/FilterFunction.java
@@ -17,19 +17,32 @@
 package io.github.seleniumquery.functions.jquery.traversing.filtering;
 
 import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 import io.github.seleniumquery.SeleniumQueryObject;
 import org.openqa.selenium.WebElement;
+
+import java.util.List;
 
 import static io.github.seleniumquery.InternalSeleniumQueryObjectFactory.instance;
 
 public class FilterFunction {
 
     public SeleniumQueryObject filter(SeleniumQueryObject seleniumQueryObject, Predicate<WebElement> filterFunction) {
+        List<WebElement> filteredWebElements = filterWebElements(seleniumQueryObject.get(), filterFunction);
         return instance().create(seleniumQueryObject.getSeleniumQueryFunctions(),
                                  seleniumQueryObject.getWebDriver(),
                                  null,
-                                 seleniumQueryObject.get(),
+                                 filteredWebElements,
                                  seleniumQueryObject);
+    }
+
+    private List<WebElement> filterWebElements(List<WebElement> unfiltered, Predicate<WebElement> filterFunction) {
+        if (filterFunction == null) {
+            return unfiltered;
+        }
+        Iterable<WebElement> filter = Iterables.filter(unfiltered, filterFunction);
+        return Lists.newArrayList(filter);
     }
 
 }
