2e03befcf27000dbc46a170106d2f48866ebf271
acdcjunior <acdcjunior@gmail.com>
2016-01-23 17:54:12 -0200

    #97 Null selector should return empty matched set

---
+++ src/test/java/io/github/seleniumquery/functions/jquery/traversing/filtering/filterfunction/FilterSelectorFunctionTest.java
@@ -0,0 +1,30 @@
+package io.github.seleniumquery.functions.jquery.traversing.filtering.filterfunction;
+
+import io.github.seleniumquery.SeleniumQueryObject;
+import org.junit.Test;
+
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.junit.Assert.*;
+import static testinfrastructure.testdouble.SeleniumQueryObjectMother.createStubSeleniumQueryObjectWithAtLeastOneElement;
+
+public class FilterSelectorFunctionTest {
+
+    private static final String NULL_SELECTOR = null;
+
+    FilterSelectorFunction filterSelectorFunction = new FilterSelectorFunction();
+
+    @Test
+    public void null_selector__should_return_EMPTY_element_set() {
+        // given
+        SeleniumQueryObject targetSQO = createStubSeleniumQueryObjectWithAtLeastOneElement();
+        // when
+        SeleniumQueryObject resultSQO = filterSelectorFunction.filter(targetSQO, NULL_SELECTOR);
+        // then
+        assertThat(resultSQO.get(), empty());
+    }
+
+    // TODO filter(this, null) --> should return empty
+    // TODO filter(this, "") --> should return empty
+    // TODO filter(this, "selector") --> should keep everyone that matches the isFunction("selector")
+
+}
