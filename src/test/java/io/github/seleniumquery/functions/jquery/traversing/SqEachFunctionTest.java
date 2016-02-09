9d96c2b4a93904c8fc9001144d1c01323de18db7
acdcjunior <acdcjunior@gmail.com>
2016-02-09 16:41:16 -0200

    #124 Implemented remaining units of $().each() [e2e tests missing now]

--- src/test/java/io/github/seleniumquery/functions/jquery/traversing/SqEachFunctionTest.java
+++ src/test/java/io/github/seleniumquery/functions/jquery/traversing/SqEachFunctionTest.java
@@ -18,12 +18,20 @@ package io.github.seleniumquery.functions.jquery.traversing;
 
 import io.github.seleniumquery.SeleniumQueryObject;
 import org.junit.Test;
+import org.openqa.selenium.WebElement;
 
-import static testinfrastructure.testdouble.io.github.seleniumquery.SeleniumQueryObjectMother.createStubSeleniumQueryObjectWithAtLeastOneElement;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static testinfrastructure.testdouble.io.github.seleniumquery.SeleniumQueryObjectMother.*;
+import static testinfrastructure.testdouble.org.openqa.selenium.WebElementMother.createWebElementWithTag;
 
 public class SqEachFunctionTest {
 
     private static final SeleniumQueryObject.EachFunction NULL_FUNCTION = null;
+    private static final SeleniumQueryObject.EachFunction NON_NULL_EACH_FUNCTION_DOES_NOT_MATTER_IN_THIS_TEST = createDummyEachFunction();
 
     private SqEachFunction sqEachFunction = new SqEachFunction();
 
@@ -35,6 +43,67 @@ public class SqEachFunctionTest {
         sqEachFunction.each(targetSQO, NULL_FUNCTION);
         // then
         // should throw exception
+    }
+
+    @Test
+    public void resultSQO_should_be_targetSQO() {
+        // given
+        SeleniumQueryObject targetSQO = createStubSeleniumQueryObjectWithAtLeastOneElement();
+        // when
+        SeleniumQueryObject resultSQO = sqEachFunction.each(targetSQO, NON_NULL_EACH_FUNCTION_DOES_NOT_MATTER_IN_THIS_TEST);
+        // then
+        assertThat(resultSQO, is(targetSQO));
+    }
+
+    @Test
+    public void should_execute_function_for_each_element_of_SQObject() {
+        // given
+        WebElement someSpan = createWebElementWithTag("span");
+        WebElement someDiv = createWebElementWithTag("div");
+
+        SeleniumQueryObject targetSQO = createStubSeleniumQueryObjectWithElements(someSpan, someDiv);
+
+        final Map<Integer, WebElement> actual = new HashMap<>();
+        SeleniumQueryObject.EachFunction eachFunctionSpy = new SeleniumQueryObject.EachFunction() {
+            @Override
+            public boolean apply(int index, WebElement element) {
+                actual.put(index, element);
+                return true;
+            }
+        };
+        // when
+        sqEachFunction.each(targetSQO, eachFunctionSpy);
+        // then
+        Map<Integer, WebElement> expected = new HashMap<>();
+        expected.put(1, someDiv);
+        expected.put(0, someSpan);
+
+        assertThat(actual, is(expected));
+    }
+
+    @Test
+    public void should_stop_execution_if_eachFunction_returns_false() {
+        // given
+        WebElement someSpan = createWebElementWithTag("span");
+        WebElement someDiv = createWebElementWithTag("div");
+
+        SeleniumQueryObject targetSQO = createStubSeleniumQueryObjectWithElements(someSpan, someDiv);
+
+        final Map<Integer, WebElement> actual = new HashMap<>();
+        SeleniumQueryObject.EachFunction eachFunctionSpy = new SeleniumQueryObject.EachFunction() {
+            @Override
+            public boolean apply(int index, WebElement element) {
+                actual.put(index, element);
+                return false;
+            }
+        };
+        // when
+        sqEachFunction.each(targetSQO, eachFunctionSpy);
+        // then
+        Map<Integer, WebElement> expected = new HashMap<>();
+        expected.put(0, someSpan);
+
+        assertThat(actual, is(expected));
     }
 
 }
