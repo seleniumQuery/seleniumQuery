f4d100b1678b8e7e4aa557f8d50a6888dcbd43d5
acdcjunior <acdcjunior@gmail.com>
2016-02-09 13:38:43 -0200

    Added e2e tests and refactored GreaterThanEvaluator

--- src/main/java/io/github/seleniumquery/wait/evaluators/GreaterThanEvaluator.java
+++ src/main/java/io/github/seleniumquery/wait/evaluators/GreaterThanEvaluator.java
@@ -1,4 +1,24 @@
+/*
+ * Copyright (c) 2016 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.seleniumquery.wait.evaluators;
+
+import io.github.seleniumquery.wait.getters.Getter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.math.BigDecimal;
 import java.text.DecimalFormat;
@@ -6,9 +26,7 @@ import java.text.NumberFormat;
 import java.text.ParseException;
 import java.util.List;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import io.github.seleniumquery.wait.getters.Getter;
+import static java.lang.String.format;
 
 public class GreaterThanEvaluator implements Evaluator<Number> {
 
@@ -26,30 +44,33 @@ public class GreaterThanEvaluator implements Evaluator<Number> {
 
 	@Override
 	public boolean evaluate(WebDriver driver, List<WebElement> elements, Number valueToCompare) {
-		BigDecimal elementNumber = null, numberToCompare = null;
+        BigDecimal numberToCompare = parseNumber(valueToCompare);
+
 		Object elementValue = getter.get(driver, elements);
-		try {
-			elementNumber = (BigDecimal) DECIMAL_FORMAT.parse(elementValue.toString());
-		} catch (ParseException e) {
-			handleException(valueToCompare, e);
-		}
-		try {
-			numberToCompare = (BigDecimal) DECIMAL_FORMAT.parse(valueToCompare.toString());
-		} catch (ParseException e) {
-			handleException(valueToCompare, e);
-		}
-		return elementNumber.compareTo(numberToCompare) > 0;
+        BigDecimal elementValueAsNumber = parseNumber(elementValue);
+
+        return elementValueAsNumber.compareTo(numberToCompare) > 0;
 	}
 
-	private void handleException(Number valueToCompare, ParseException e) {
-		throw new RuntimeException("Error parsing "+valueToCompare+" into a number. The conversion function uses " +
-				"the default Locale. If your number uses a diferent format, try changing the default Locale " +
-				"through \"Locale.setDefaultLocale(Locale.<SOME_OTHER_LOCALE>);\".", e);
+    private BigDecimal parseNumber(Object elementValue) {
+        try {
+            return (BigDecimal) DECIMAL_FORMAT.parse(elementValue.toString());
+        } catch (ParseException e) {
+            throw handleException(elementValue, e);
+        }
+    }
+
+    private IllegalArgumentException handleException(Object failedParsingObject, ParseException e) {
+		return new IllegalArgumentException(format(
+				"Error parsing \"%s\" into a number. " +
+                "The conversion function uses the default Locale. " +
+                "If your number uses a diferent format, try changing the default Locale through \"Locale.setDefaultLocale(Locale.<SOME_OTHER_LOCALE>);\".",
+                failedParsingObject), e);
 	}
 
 	@Override
 	public String stringFor(Number valueToCompare) {
-		return getter + " isGreaterThan(\"" + valueToCompare + "\")";
+		return String.format("%s isGreaterThan(\"%s\")", getter, valueToCompare);
 	}
 
 }
