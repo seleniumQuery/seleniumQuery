54a0924abcebbeebfa50845a9cd9899025329433
acdcjunior <acdcjunior@gmail.com>
2016-02-09 14:27:05 -0200

    General minor adjustments on e2e tests of evaluators

--- src/test/java/endtoend/waitUntil/WaitUntilIsMoreTest.java
+++ src/test/java/endtoend/waitUntil/WaitUntilIsMoreTest.java
@@ -1,18 +1,33 @@
+/*
+ * Copyright (c) 2016 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package endtoend.waitUntil;
 
-import testinfrastructure.junitrule.SetUpAndTearDownDriver;
 import io.github.seleniumquery.wait.SeleniumQueryTimeoutException;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import testinfrastructure.junitrule.SetUpAndTearDownDriver;
 
 import static io.github.seleniumquery.SeleniumQuery.$;
 import static org.junit.Assert.assertEquals;
 
 public class WaitUntilIsMoreTest {
 
-	@ClassRule public static SetUpAndTearDownDriver setUpAndTearDownDriverRule = new SetUpAndTearDownDriver();
-	@Rule public SetUpAndTearDownDriver setUpAndTearDownDriverRuleInstance = setUpAndTearDownDriverRule;
+	@ClassRule @Rule public static SetUpAndTearDownDriver setUpAndTearDownDriverRule = new SetUpAndTearDownDriver();
 
 	@Test
 	public void isPresent() {
--- src/test/java/endtoend/waitUntil/WaitUntilIsPresentTest.java
+++ src/test/java/endtoend/waitUntil/WaitUntilIsPresentTest.java
@@ -1,22 +1,36 @@
+/*
+ * Copyright (c) 2016 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package endtoend.waitUntil;
+
+import org.junit.ClassRule;
+import org.junit.Rule;
+import org.junit.Test;
+import testinfrastructure.junitrule.JavaScriptOnly;
+import testinfrastructure.junitrule.SetUpAndTearDownDriver;
 
 import static io.github.seleniumquery.SeleniumQuery.$;
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
-import testinfrastructure.junitrule.JavaScriptOnly;
-import testinfrastructure.junitrule.SetUpAndTearDownDriver;
-
-import org.junit.ClassRule;
-import org.junit.Rule;
-import org.junit.Test;
-
 public class WaitUntilIsPresentTest {
 
-	@ClassRule public static SetUpAndTearDownDriver setUpAndTearDownDriverRule = new SetUpAndTearDownDriver();
-	@Rule public SetUpAndTearDownDriver setUpAndTearDownDriverRuleInstance = setUpAndTearDownDriverRule;
-	
+	@ClassRule @Rule public static SetUpAndTearDownDriver setUpAndTearDownDriverRule = new SetUpAndTearDownDriver();
+
 	@Test @JavaScriptOnly
 	public void isPresent__should_not_throw_an_exception_when_the_element_becomes_present__and__return_the_elements_val() throws Exception {
 		// given
@@ -28,22 +42,22 @@ public class WaitUntilIsPresentTest {
 	}
 	
 	@Test
-	public void queryUntil_not_present() {
+	public void waitUntil_not_present() {
 		$(".whatever").waitUntil().is(":not(:present)");
 	}
 	
 	@Test
-	public void queryUntil_enabled_not_present() {
+	public void waitUntil_enabled_not_present() {
 		$(".whatever").waitUntil().is(":enabled:not(:present)");
 	}
 	
 	@Test
-	public void queryUntil_not_present_enabled() {
+	public void waitUntil_not_present_enabled() {
 		$(".whatever").waitUntil().is(":not(:present):enabled");
 	}
 	
 	@Test
-	public void queryUntil_not_not_not_present_with_others() {
+	public void waitUntil_not_not_not_present_with_others() {
 		$(".whatever").waitUntil().is(".xyz:enabled:not(:not(:not(:present))):disabled");
 	}
 	
