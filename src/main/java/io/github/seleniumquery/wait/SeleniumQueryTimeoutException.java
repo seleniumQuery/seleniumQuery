c78a42fdb909645d5ca82f21f13e169b838fa128
acdcjunior <acdcjunior@gmail.com>
2016-02-10 15:35:37 -0200

    Visibility change #129

--- src/main/java/io/github/seleniumquery/wait/SeleniumQueryTimeoutException.java
+++ src/main/java/io/github/seleniumquery/wait/SeleniumQueryTimeoutException.java
@@ -1,24 +1,40 @@
+/*
+ * Copyright (c) 2016 seleniumQuery authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.github.seleniumquery.wait;
+
+import io.github.seleniumquery.SeleniumQueryConfig;
+import io.github.seleniumquery.SeleniumQueryObject;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.TimeoutException;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import org.apache.commons.io.FileUtils;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
-import org.openqa.selenium.TimeoutException;
-import io.github.seleniumquery.SeleniumQueryConfig;
-import io.github.seleniumquery.SeleniumQueryObject;
-
 public class SeleniumQueryTimeoutException extends TimeoutException {
 
-	private static final long serialVersionUID = 2L;
+	private static final long serialVersionUID = 3L;
 	
 	private SeleniumQueryObject seleniumQueryObject;
 	
-	public SeleniumQueryTimeoutException(TimeoutException sourceException, SeleniumQueryObject seleniumQueryObject, String reason) {
+	SeleniumQueryTimeoutException(TimeoutException sourceException, SeleniumQueryObject seleniumQueryObject, String reason) {
 		super("Timeout while waiting for selector '"+seleniumQueryObject.getBy()+"' "+reason, sourceException);
 		this.seleniumQueryObject = seleniumQueryObject;
 		
