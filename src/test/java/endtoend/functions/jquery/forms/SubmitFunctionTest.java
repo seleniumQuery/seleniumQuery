6df2e69eac08b2194de03456e4ff99d8c724d1f5
acdcjunior <acdcjunior@gmail.com>
2016-03-26 14:42:28 -0300

    test(functions): remove unused import

--- src/test/java/endtoend/functions/jquery/forms/SubmitFunctionTest.java
+++ src/test/java/endtoend/functions/jquery/forms/SubmitFunctionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 seleniumQuery authors
+ * Copyright (c) 2016 seleniumQuery authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,12 +16,11 @@
 
 package endtoend.functions.jquery.forms;
 
-import org.junit.Before;
-import testinfrastructure.junitrule.JavaScriptOnly;
-import testinfrastructure.junitrule.SetUpAndTearDownDriver;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import testinfrastructure.junitrule.JavaScriptOnly;
+import testinfrastructure.junitrule.SetUpAndTearDownDriver;
 
 import static io.github.seleniumquery.SeleniumQuery.$;
 import static org.hamcrest.CoreMatchers.is;
