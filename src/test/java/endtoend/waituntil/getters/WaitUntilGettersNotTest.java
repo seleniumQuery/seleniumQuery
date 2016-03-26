e6556a7c5523d24bc004ec4e9cd936d6b6ec27e1
acdcjunior <acdcjunior@gmail.com>
2016-03-26 14:23:47 -0300

    test(waitUntil): remove duplicated test methods

--- src/test/java/endtoend/waituntil/getters/WaitUntilGettersNotTest.java
+++ src/test/java/endtoend/waituntil/getters/WaitUntilGettersNotTest.java
@@ -33,14 +33,12 @@ import static org.junit.Assert.assertThat;
 
 public class WaitUntilGettersNotTest {
 
-    @ClassRule
-    @Rule
-    public static SetUpAndTearDownDriver setUpAndTearDownDriverRule = new SetUpAndTearDownDriver(WaitUntilGettersTest.class);
+    @ClassRule @Rule public static SetUpAndTearDownDriver setUpAndTearDownDriverRule = new SetUpAndTearDownDriver(WaitUntilGettersTest.class);
 
     private List<WebElement> divs;
 
     @Before
-    public void setUp() throws Exception {
+    public void setUp() {
         divs = $("#d1,#d2").get();
     }
 
@@ -69,58 +67,6 @@ public class WaitUntilGettersNotTest {
     @Test
     public void text_getter() {
         assertThat($("div").waitUntil().text().not().isEqualTo("NOT").then().get(), is(divs));
-    }
-
-    @Test
-    public void size_getter_toString() {
-        // when
-        SeleniumQueryWaitAndOrThen waitAndOrThen = $("div").waitUntil().size().isEqualTo(2);
-        // then
-        String expectedToString = "$(\"div\").waitUntil().size().isEqualTo(2)";
-        assertThat(waitAndOrThen.toString(), is(expectedToString));
-        assertThat(waitAndOrThen.then().toString(), is(expectedToString));
-    }
-
-    @Test
-    @JavaScriptOnly
-    public void attr_getter_toString() {
-        // when
-        SeleniumQueryWaitAndOrThen waitAndOrThen = $("div").waitUntil().attr("data-attr").isEqualTo("data-attr-value");
-        // then
-        String expectedToString = "$(\"div\").waitUntil().attr(\"data-attr\").isEqualTo(\"data-attr-value\")";
-        assertThat(waitAndOrThen.toString(), is(expectedToString));
-        assertThat(waitAndOrThen.then().toString(), is(expectedToString));
-    }
-
-    @Test
-    @JavaScriptOnly
-    public void prop_getter_toString() {
-        // when
-        SeleniumQueryWaitAndOrThen waitAndOrThen = $("div").waitUntil().prop("tagName").isEqualTo("DIV");
-        // then
-        String expectedToString = "$(\"div\").waitUntil().prop(\"tagName\").isEqualTo(\"DIV\")";
-        assertThat(waitAndOrThen.toString(), is(expectedToString));
-        assertThat(waitAndOrThen.then().toString(), is(expectedToString));
-    }
-
-    @Test
-    public void html_getter_toString() {
-        // when
-        SeleniumQueryWaitAndOrThen waitAndOrThen = $("div").waitUntil().html().isEqualTo("&gt;&lt;");
-        // then
-        String expectedToString = "$(\"div\").waitUntil().html().isEqualTo(\"&gt;&lt;\")";
-        assertThat(waitAndOrThen.toString(), is(expectedToString));
-        assertThat(waitAndOrThen.then().toString(), is(expectedToString));
-    }
-
-    @Test
-    public void text_getter_toString() {
-        // when
-        SeleniumQueryWaitAndOrThen waitAndOrThen = $("div").waitUntil().text().contains("< >");
-        // then
-        String expectedToString = "$(\"div\").waitUntil().text().contains(\"< >\")";
-        assertThat(waitAndOrThen.toString(), is(expectedToString));
-        assertThat(waitAndOrThen.then().toString(), is(expectedToString));
     }
 
     @Test
