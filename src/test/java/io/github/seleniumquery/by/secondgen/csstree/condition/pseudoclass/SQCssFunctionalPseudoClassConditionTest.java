c0397a801901aea9fcadcccdc68b8fc1665916c5
acdcjunior <acdcjunior@gmail.com>
2016-03-26 14:56:43 -0300

    refactor(selectors.secondgen): replace == with .equals()

--- src/test/java/io/github/seleniumquery/by/secondgen/csstree/condition/pseudoclass/SQCssFunctionalPseudoClassConditionTest.java
+++ src/test/java/io/github/seleniumquery/by/secondgen/csstree/condition/pseudoclass/SQCssFunctionalPseudoClassConditionTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2015 seleniumQuery authors
+ * Copyright (c) 2016 seleniumQuery authors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -42,8 +42,9 @@ public class SQCssFunctionalPseudoClassConditionTest {
         final SQCssConditionImplementedFinders finderGen = new SQCssConditionImplementedFinders() {
             @Override
             public ElementFinder toElementFinder(ElementFinder leftFinder) {
-                if (leftFinder == argFinder)
+                if (argFinder.equals(leftFinder)) {
                     return returningFinder;
+                }
                 return null;
             }
         };
