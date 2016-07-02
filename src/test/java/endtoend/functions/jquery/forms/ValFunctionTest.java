bc6fa3ed58a3cd0ae61d8d700e7c7c66431b02ba
acdcjunior <acdcjunior@gmail.com>
2016-07-02 03:47:29 -0300

    feat(functions.val): suppress any errors when attempt to clear + add more tests for .val() and iframes
    [run sauce]
    #166

--- src/test/java/endtoend/functions/jquery/forms/ValFunctionTest.java
+++ src/test/java/endtoend/functions/jquery/forms/ValFunctionTest.java
@@ -16,6 +16,7 @@
 
 package endtoend.functions.jquery.forms;
 
+import org.hamcrest.Matcher;
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
@@ -23,14 +24,10 @@ import org.openqa.selenium.ElementNotVisibleException;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import testinfrastructure.junitrule.SetUpAndTearDownDriver;
-import testinfrastructure.junitrule.annotation.ChromeOnly;
-import testinfrastructure.junitrule.annotation.ChromeShouldBeSkipped;
-import testinfrastructure.junitrule.annotation.JavaScriptDisabledOnly;
-import testinfrastructure.junitrule.annotation.JavaScriptEnabledOnly;
+import testinfrastructure.junitrule.annotation.*;
 
 import static io.github.seleniumquery.SeleniumQuery.$;
-import static org.hamcrest.Matchers.is;
-import static org.hamcrest.Matchers.startsWith;
+import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 import static testinfrastructure.testutils.DriverInTest.*;
@@ -193,30 +190,76 @@ public class ValFunctionTest {
         return resultingHtml;
     }
 
+    /**
+     * IE can't read the values properly. Gives null for .val() and "" for .text().
+     */
     @Test
-    public void iframe_with_DesignMode_ON___defaultValues__are__read__correctly() {
-        $.driver().get().switchTo().frame("iframe-with-design-mode-on");
-        assertThat($("body").val(), is(""));
-        assertThat($("body").text(), is("srcdoc-on"));
-    }
+    @ChromeOnly
+    public void iframe_with_DesignMode_ON___defaultValues__are__read__correctly() { verifyIframeTextRead("iframe-body-content"); }
 
     @Test
+    @FirefoxOnly
+    public void iframe_with_DesignMode_ON___defaultValues__are__read__correctly_ff() { verifyIframeTextRead("iframe-body-content"); }
+
+    @Test
+    @SafariOnly
+    public void iframe_with_DesignMode_ON___defaultValues__are__read__correctly_safari() { verifyIframeTextRead("iframe-body-content"); }
+
+    @Test
+    @EdgeOnly
+    public void iframe_with_DesignMode_ON___defaultValues__are__read__correctly__EDGE() { verifyIframeTextRead("iframe-body-content "); }
+
+    private void verifyIframeTextRead(String expectedIframeText) {
+        $.url("http://rawgit.com/seleniumQuery/seleniumQuery/master/src/test/java/endtoend/functions/jquery/forms/ValFunctionTest3.html");
+        $.driver().get().switchTo().frame("iframe-with-design-mode-on");
+        assertThat($("body").text(), is(expectedIframeText));
+        assertThat($("body").val(), is(""));
+    }
+
+    /**
+     * IE simply dies when we try to FETCH an element from the iframe. If gives no stacktrace info.
+     * This had to be done for IE to work: $.driver().get().switchTo().frame(0);
+     * They suggested, but didnt have effect: WebElement editable =  $.driver().get().switchTo().activeElement();
+     * Also suggested $.driver().get().switchTo().defaultContent(); before switching to frame. No use.
+     *
+     *
+     * Safari dies when typing.
+     * Firefox does not clear selection.
+     * Edge does not read the value correctly (nor types).
+     * Chrome works 100%.
+     * HtmlUnit reads, but doesnt type.
+     * PhantomJS same as Edge.
+     */
+    @Test
     @JavaScriptEnabledOnly
-    public void iframe_with_DesignMode_ON___values_are_CHANGED_correctly() {
+    @ChromeOnly
+    public void iframe_with_DesignMode_ON___values_are_CHANGED_correctly__CHROME() {
+        verifyTypingAtIframeChangesValueAsExpected(is("[typed-value]"));
+    }
+    @Test
+    @JavaScriptEnabledOnly
+    @FirefoxOnly
+    public void iframe_with_DesignMode_ON___values_are_CHANGED_correctly__FIREFOX() {
+        verifyTypingAtIframeChangesValueAsExpected(allOf(startsWith("[typed-value]"), not(is("[typed-value]"))));
+    }
+
+    private void verifyTypingAtIframeChangesValueAsExpected(Matcher<String> resultTextMatcher) {
+        $.url("http://rawgit.com/seleniumQuery/seleniumQuery/master/src/test/java/endtoend/functions/jquery/forms/ValFunctionTest3.html");
         $("#iframe-debug").waitUntil().text().isEqualTo("designMode successfully set to on by JavaScript");
         $.driver().get().switchTo().frame("iframe-with-design-mode-on");
-        $("body").val("bob");
+        $("body").val("[typed-value]");
         assertThat($("body").val(), is(""));
-        assertThat($("body").text(), is("bob"));
+        assertThat($("body").text(), resultTextMatcher);
     }
 
     @Test
     @JavaScriptDisabledOnly
+    @FirefoxOnly
     public void iframe_with_DesignMode_ON____JS_OFF____values_DONT_change__but_no_exception_is_thrown_as_well() {
         $.driver().get().switchTo().frame("iframe-with-design-mode-on");
-        $("body").val("bob");
+        $("body").val("something");
         assertThat($("body").val(), is(""));
-        assertThat($("body").text(), is("srcdoc-on"));
+        assertThat($("body").text(), is("iframe-body-content"));
 
         $.driver().get().switchTo().defaultContent();
         assertThat($("#iframe-debug").text(), is("no-javascript")); // make sure JS didn't run!
