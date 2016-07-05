9e0c2576ffd9977c420990356c53df4f20367925
acdcjunior <acdcjunior@gmail.com>
2016-07-04 23:41:21 -0300

    test(e2e): implement @OperaSkip

---
+++ src/test/java/testinfrastructure/junitrule/annotation/OperaSkip.java
@@ -0,0 +1,17 @@
+package testinfrastructure.junitrule.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * Specifies that the test method should NOT be run when the driver is Edge.
+ */
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.TYPE})
+public @interface OperaSkip {
+
+    String value() default "";
+
+}
