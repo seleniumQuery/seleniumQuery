bfcd3d80e4109c338e27e01b987dd2991fc6db0a
acdcjunior <acdcjunior@gmail.com>
2016-07-11 23:17:43 -0300

    test(selector.secondgen): add guard to make test work for both gens

--- src/test/java/endtoend/selectors/pseudoclasses/form/CheckboxPseudoClassTest.java
+++ src/test/java/endtoend/selectors/pseudoclasses/form/CheckboxPseudoClassTest.java
@@ -1,27 +1,25 @@
 package endtoend.selectors.pseudoclasses.form;
 
-import static io.github.seleniumquery.SeleniumQuery.$;
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-import testinfrastructure.junitrule.SetUpAndTearDownDriver;
-
 import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
+import testinfrastructure.SecondGenSelectorSystemDetector;
+import testinfrastructure.junitrule.SetUpAndTearDownDriver;
+
+import static io.github.seleniumquery.SeleniumQuery.$;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class CheckboxPseudoClassTest {
 
-	@ClassRule public static SetUpAndTearDownDriver setUpAndTearDownDriverRule = new SetUpAndTearDownDriver();
-	@Rule public SetUpAndTearDownDriver setUpAndTearDownDriverRuleInstance = setUpAndTearDownDriverRule;
-	
+    @ClassRule @Rule public static SetUpAndTearDownDriver setUpAndTearDownDriverRule = new SetUpAndTearDownDriver();
+
 	@Test
 	public void checkboxPseudoClass() {
 		assertThat($("[type='checkbox']").size(), is(4));
 		assertThat($(":checkbox").size(), is(1));
 		assertThat($("*:checkbox").size(), is(1));
 		assertThat($("input:checkbox").size(), is(1));
-		assertThat($("div:checkbox").size(), is(0));
-		assertThat($("span:checkbox").size(), is(0));
 
 		assertThat($("#i1").is(":checkbox"), is(true));
 		assertThat($("#i1").is("*:checkbox"), is(true));
@@ -31,5 +29,10 @@ public class CheckboxPseudoClassTest {
 		assertThat($("#i3").is(":checkbox"), is(false));
 		assertThat($("#i4").is(":checkbox"), is(false));
 	}
-	
+
+	@Test
+	public void checkboxPseudoClass__invalid() {
+        SecondGenSelectorSystemDetector.assertPseudoOnDivAndSpanIsEmptyOn1stGenAndThrowsExceptionOn2ndGen(":checkbox");
+	}
+
 }
