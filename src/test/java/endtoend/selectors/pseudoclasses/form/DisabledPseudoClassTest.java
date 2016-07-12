bd5ba9a98c936d1c6b1e248c5b5952e06829e1ac
acdcjunior <acdcjunior@gmail.com>
2016-07-11 23:19:33 -0300

    test(e2e): add clarifying assertions

--- src/test/java/endtoend/selectors/pseudoclasses/form/DisabledPseudoClassTest.java
+++ src/test/java/endtoend/selectors/pseudoclasses/form/DisabledPseudoClassTest.java
@@ -28,30 +28,32 @@ import static testinfrastructure.EndToEndTestUtils.t;
 
 public class DisabledPseudoClassTest {
 
-	@ClassRule public static SetUpAndTearDownDriver setUpAndTearDownDriverRule = new SetUpAndTearDownDriver();
-	@Rule public SetUpAndTearDownDriver setUpAndTearDownDriverRuleInstance = setUpAndTearDownDriverRule;
-	
+    @ClassRule @Rule public static SetUpAndTearDownDriver setUpAndTearDownDriverRule = new SetUpAndTearDownDriver();
+
 	// http://jsbin.com/guqef/3/edit
 	@Test
 	public void disabledPseudo_with_tag_button() {
 		assertThat($("button:disabled").size(), is(2));
 	}
-	
+
 	@Test
 	public void disabledPseudo_with_tag_input() {
 		assertThat($("input:disabled").size(), is(48));
 	}
-	
+
 	@Test
 	public void disabledPseudo_with_tag_select() {
 		assertThat($("select:disabled").size(), is(2));
 	}
-	
+
 	@Test
 	public void disabledPseudo_with_tag_option() {
+		assertThat($("option").size(), is(27));
 		assertThat($("option:disabled").size(), is(24));
+		assertThat($("option:contains(ENABLED)").size(), is(3));
+		assertThat($("option:disabled:contains(disabled)").size(), is(24));
 	}
-	
+
 	@Test
 	public void disabledPseudo_with_tag_optgroup() {
 		t(":disabled <optgroup>s", "optgroup:disabled", "optgroup-2", "optgroup-3", "optgroup-5", "optgroup-6", "optgroup-8", "optgroup-9");
@@ -61,7 +63,7 @@ public class DisabledPseudoClassTest {
 	public void disabledPseudo_with_tag_textarea() {
 		assertThat($("textarea:disabled").size(), is(2));
 	}
-	
+
 	@Test
 	public void disabledPseudo() {
 		assertThat($(":disabled").size(), is(84));
