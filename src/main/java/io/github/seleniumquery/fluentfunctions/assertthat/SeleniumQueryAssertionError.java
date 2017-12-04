61c935f55f6384d0b4a3b969ff8d2fbdec177183
Antonio Jr <acdcjunior@gmail.com>
2017-12-04 17:16:43 -0200

    feat(assertthat): create SeleniumQueryAssertionError

---
+++ src/main/java/io/github/seleniumquery/fluentfunctions/assertthat/SeleniumQueryAssertionError.java
@@ -0,0 +1,9 @@
+package io.github.seleniumquery.fluentfunctions.assertthat;
+
+public class SeleniumQueryAssertionError extends AssertionError {
+
+    public SeleniumQueryAssertionError(String detail) {
+        super(detail);
+    }
+
+}
