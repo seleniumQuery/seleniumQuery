ef4361e1aa0b121cc34dc64d0cbdd0850f1479f7
Antonio Jr <acdcjunior@gmail.com>
2017-12-17 13:40:56 -0200

    test(infra): add banner so we know which CI was detected

--- src/test/java/testinfrastructure/junitrule/config/EndToEndTestConfig.java
+++ src/test/java/testinfrastructure/junitrule/config/EndToEndTestConfig.java
@@ -6,28 +6,43 @@ public class EndToEndTestConfig {
 
     public static DriverToRunTestsIn whatDriversShouldTestsRun() {
         if (EnvironmentTestUtils.isRunningAtTravis()) {
+            banner("travis");
             return DriverToRunTestsIn.PHANTOMJS;
         }
         if (EnvironmentTestUtils.isRunningAtAppveyor()) {
+            banner("appveyor");
             return DriverToRunTestsIn.FIREFOX;
         }
         if (EnvironmentTestUtils.isRunningAtCircleCi()) {
+            banner("circleci");
             return DriverToRunTestsIn.HTMLUNIT_ALL_JS_OFF_ONLY;
         }
         if (EnvironmentTestUtils.isRunningAtShippable()) {
+            banner("shippable");
             return DriverToRunTestsIn.CHROME;
         }
         if (EnvironmentTestUtils.isRunningAtCodeShip()) {
+            banner("codeship");
             // will also run DriverToRunTestsIn.REMOTE if last commit message contains [run sauce]
             return DriverToRunTestsIn.HTMLUNIT_CHROME_JS_ON_ONLY;
         }
         if (EnvironmentTestUtils.isRunningAtWercker()) {
+            banner("wercker");
             return DriverToRunTestsIn.HTMLUNIT_ALL_JS_ON_ONLY;
         }
         if (EnvironmentTestUtils.isRunningAtContinuousIntegrationServer()) {
+            banner("any other CI");
             return DriverToRunTestsIn.HEADLESS_DRIVERS_JS_ON_AND_OFF;
         }
-        return DriverToRunTestsIn.HTMLUNIT_CHROME_JS_ON_ONLY;
+        return DriverToRunTestsIn.HEADLESS_DRIVERS_JS_ON_ONLY;
+    }
+
+    private static void banner(String environment) {
+        System.out.println("###############################################################################################");
+        System.out.println("###############################################################################################");
+        System.out.println("### Environment detected: " + environment);
+        System.out.println("###############################################################################################");
+        System.out.println("###############################################################################################");
     }
 
 }
